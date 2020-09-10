c0acc45dcbeca4f4452fd924bfde237b1cbc7a9a
Hakan Ergun <hakan.ergun@ieee.org>
2020-09-10 13:31:00 +0200

    Add comments to test scripts for explanation

--- test/scripts/tests_flex_demand.jl
+++ test/scripts/tests_flex_demand.jl
@@ -1,9 +1,15 @@
+# Import relevant pakcages:
+# FlexPlan uses PowerModelsACDC for the multi-period transmission expansion optimisation & DC grid
+# PowerModelsACDC uses PowerModels for the AC grid, and the optimisation create_profile_data
+# InfrastructureModels is needed for data manipulation and common functions
 import FlexPlan; const _FP = FlexPlan
 import PowerModelsACDC; const _PMACDC = PowerModelsACDC
 import PowerModels; const _PM = PowerModels
 import InfrastructureModels; const _IM = InfrastructureModels
+
+# Add solver packages,, NOTE: packages are needed handle communication bwteeen solver and Julia/JuMP, 
+# they don't include the solver itself (the commercial ones). For instance ipopt, Cbc, juniper and so on should work
 import Ipopt
-# using CPLEX
 import SCS
 import Juniper
 import Mosek
@@ -13,6 +19,7 @@ import Gurobi
 import Cbc
 import CPLEX
 
+# Solver configurations
 scs = JuMP.with_optimizer(SCS.Optimizer, max_iters=100000)
 ipopt = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-4, print_level=0)
 cplex = JuMP.with_optimizer(CPLEX.Optimizer)
@@ -22,27 +29,29 @@ mosek = JuMP.with_optimizer(Mosek.Optimizer)
 juniper = JuMP.with_optimizer(Juniper.Optimizer, nl_solver = ipopt, mip_solver= cbc, time_limit= 7200)
 
 
-dim = 4
-file = "./test/data/case6_flex.m"
-# Test 1: Line vs storage: Base case
-data = _PM.parse_file(file)
-_PMACDC.process_additional_data!(data)
-loadprofile = ones(5, dim)
+# TEST SCRIPT to run multi-period optimisation of demand flexibility, AC & DC lines and storage investments 
 
-_FP.add_storage_data!(data)
-_FP.add_flexible_demand_data!(data)
-loadprofile[end, :] = repeat([100 100 100 240] / 240, 1 , Int(dim /4))
-extradata = _FP.create_profile_data(dim, data, loadprofile)
+# Input parameters:
+dim = 4   # Number of time points
+file = "./test/data/case6_flex.m" # Input case, in matpower m-file format: Here 6bus case with candidate AC, DC lines, flexible demand and candidate storage
+loadprofile = ones(5, dim) # Create a load profile: In this case there are 5 loads in the test case
+# Manipultate load profile: Load number 5 changes over time: Orignal load is 240 MW.
+loadprofile[end, :] = repeat([100 100 100 240] / 240, 1 , Int(dim /4)) 
+
+# Data manipulation (per unit conversions and matching data models)
+data = _PM.parse_file(file)  # Create PowerModels data dictionary (AC networks and storage)
+_PMACDC.process_additional_data!(data) # Add DC grid data to the data dictionary
+_FP.add_storage_data!(data) # Add addtional storage data model
+_FP.add_flexible_demand_data!(data) # Add flexible data model
+
+extradata = _FP.create_profile_data(dim, data, loadprofile) # create a dictionary to pass time series data to data dictionary
+# Create data dictionary where time series data is included at the right place
 mn_data = _PMACDC.multinetwork_data(data, extradata, Set{String}(["source_type", "name", "source_version", "per_unit"]))
 
+# Add PowerModels(ACDC) settings
 s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => false, "process_data_internally" => false)
+
+# Build optimisation model, solve it and write solution dictionary:
+# This is the "problem file" which needs to be constructed individually depending on application
+# In this case: multi-period optimisation of demand flexibility, AC & DC lines and storage investments
 result_test1 = _FP.flex_tnep(mn_data, _PM.DCPPowerModel, gurobi, multinetwork=true; setting = s)
-
-
-
-# TODO
-# New problem type flex_demand
-# STEP 1: Demand flexibility model -> OPEX only
-# -- Add time shifting constraint
-# -- .......
-# SETTINGS for the new problem types?
--- test/scripts/tests_strg.jl
+++ test/scripts/tests_strg.jl
@@ -1,9 +1,15 @@
+# Import relevant pakcages:
+# FlexPlan uses PowerModelsACDC for the multi-period transmission expansion optimisation & DC grid
+# PowerModelsACDC uses PowerModels for the AC grid, and the optimisation create_profile_data
+# InfrastructureModels is needed for data manipulation and common functions
 import FlexPlan; const _FP = FlexPlan
 import PowerModelsACDC; const _PMACDC = PowerModelsACDC
 import PowerModels; const _PM = PowerModels
 import InfrastructureModels; const _IM = InfrastructureModels
+
+# Add solver packages,, NOTE: packages are needed handle communication bwteeen solver and Julia/JuMP, 
+# they don't include the solver itself (the commercial ones). For instance ipopt, Cbc, juniper and so on should work
 import Ipopt
-# using CPLEX
 import SCS
 import Juniper
 import Mosek
@@ -13,6 +19,7 @@ import Gurobi
 import Cbc
 import CPLEX
 
+# Solver configurations
 scs = JuMP.with_optimizer(SCS.Optimizer, max_iters=100000)
 ipopt = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-4, print_level=0)
 cplex = JuMP.with_optimizer(CPLEX.Optimizer)
@@ -22,44 +29,66 @@ mosek = JuMP.with_optimizer(Mosek.Optimizer)
 juniper = JuMP.with_optimizer(Juniper.Optimizer, nl_solver = ipopt, mip_solver= cbc, time_limit= 7200)
 
 
-dim = 4
-file = "./test/data/case6_strg.m"
-# Test 1: Line vs storage: Base case
-data = _PM.parse_file(file)
-_PMACDC.process_additional_data!(data)
-loadprofile = ones(5, dim)
+# TEST SCRIPT to run multi-period optimisation of demand flexibility, AC & DC lines and storage investments 
+# Input parameters
+dim = 4 # Number of time points
+file = "./test/data/case6_strg.m"  #Input case, in matpower m-file format: Here 6bus case with candidate AC, DC lines and candidate storage
 
-_FP.add_storage_data!(data)
-extradata = _FP.create_profile_data(dim, data, loadprofile)
+
+# Test 1: Line vs storage: Base case Load at bus 5 [240, 240, 240 , 240] MW over time
+data = _PM.parse_file(file) # Create PowerModels data dictionary (AC networks and storage)
+_PMACDC.process_additional_data!(data) # Add DC grid data to the data dictionary
+_FP.add_storage_data!(data) # Add addtional storage data model
+loadprofile = ones(5, dim) # Create a load profile: In this case there are 5 loads in the test case
+
+
+extradata = _FP.create_profile_data(dim, data, loadprofile) # create a dictionary to pass time series data to data dictionary
+# Create data dictionary where time series data is included at the right place
 mn_data = _PMACDC.multinetwork_data(data, extradata, Set{String}(["source_type", "name", "source_version", "per_unit"]))
 
+# Add PowerModels(ACDC) settings
 s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => false, "process_data_internally" => false)
+# Build optimisation model, solve it and write solution dictionary:
+# This is the "problem file" which needs to be constructed individually depending on application
+# In this case: multi-period optimisation of demand flexibility, AC & DC lines and storage investments
+
 result_test1 = _FP.strg_tnep(mn_data, _PM.DCPPowerModel, gurobi, multinetwork=true; setting = s)
 
-# Test 2: Line vs storage: Load at bus 5 [100, 100, 100 , 240 MW]
-data = _PM.parse_file(file)
-_PMACDC.process_additional_data!(data)
-loadprofile = ones(5, dim)
+# Test 2: Line vs storage: Load at bus 5 [100, 100, 100 , 240] MW over time
+data = _PM.parse_file(file) # Create PowerModels data dictionary (AC networks and storage)
+_PMACDC.process_additional_data!(data) # Add DC grid data to the data dictionary
+_FP.add_storage_data!(data) # Add addtional storage data model
+loadprofile = ones(5, dim) # Create a load profile: In this case there are 5 loads in the test case
 loadprofile[end, :] = repeat([100 100 100 240] / 240, 1 , Int(dim /4))
 
-_FP.add_storage_data!(data)
-extradata = _FP.create_profile_data(dim, data, loadprofile)
+
+extradata = _FP.create_profile_data(dim, data, loadprofile) # create a dictionary to pass time series data to data dictionary
+# Create data dictionary where time series data is included at the right place
 mn_data = _PMACDC.multinetwork_data(data, extradata, Set{String}(["source_type", "name", "source_version", "per_unit"]))
 
+# Add PowerModels(ACDC) settings
 s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => false, "process_data_internally" => false)
+# Build optimisation model, solve it and write solution dictionary:
+# This is the "problem file" which needs to be constructed individually depending on application
+# In this case: multi-period optimisation of demand flexibility, AC & DC lines and storage investments
 result_test2 = _FP.strg_tnep(mn_data, _PM.DCPPowerModel, gurobi, multinetwork=true; setting = s)
 
-# Test 3: Line vs storage: Storage investment -> existing storage is out
-data = _PM.parse_file(file)
-_PMACDC.process_additional_data!(data)
-
-loadprofile = ones(5, dim)
+# Test 3: Line vs storage: Storage investment -> existing storage is out of service "status" = 0
+data = _PM.parse_file(file) # Create PowerModels data dictionary (AC networks and storage)
+_PMACDC.process_additional_data!(data) # Add DC grid data to the data dictionary
+_FP.add_storage_data!(data) # Add addtional storage data model
+data["storage"]["1"]["status"] = 0 # take existing storage out of service
+loadprofile = ones(5, dim) # Create a load profile: In this case there are 5 loads in the test case
 loadprofile[end, :] = repeat([100 100 100 240] / 240, 1 , Int(dim /4))
 
-_FP.add_storage_data!(data)
-data["storage"]["1"]["status"] = 0
-extradata = _FP.create_profile_data(dim, data, loadprofile)
+
+extradata = _FP.create_profile_data(dim, data, loadprofile) # create a dictionary to pass time series data to data dictionary
+# Create data dictionary where time series data is included at the right place
 mn_data = _PMACDC.multinetwork_data(data, extradata, Set{String}(["source_type", "name", "source_version", "per_unit"]))
 
+# Add PowerModels(ACDC) settings
 s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => false, "process_data_internally" => false)
+# Build optimisation model, solve it and write solution dictionary:
+# This is the "problem file" which needs to be constructed individually depending on application
+# In this case: multi-period optimisation of demand flexibility, AC & DC lines and storage investments
 result_test3 = _FP.strg_tnep(mn_data, _PM.DCPPowerModel, gurobi, multinetwork=true; setting = s)
