f734e6c73b89bf7238b41b389ad052c64d6e3aff
Matteo Rossini <matteo.rossini@rse-web.it>
2020-11-11 18:24:09 +0100

    Dropped TNEP support
    Bus KCL constraints in the presence of candidate lines cannot be easily implemented when line shunt admittances are modeled as bus shunt loads.

--- src/form/lacrad.jl
+++ src/form/lacrad.jl
@@ -13,10 +13,6 @@ function _PM.variable_bus_voltage_magnitude_only(pm::LACRadPowerModel; kwargs...
     _PM.variable_bus_voltage_magnitude_sqr(pm; kwargs...)
 end
 
-"nothing to add, there are no voltage variables on branches"
-function _PM.variable_ne_branch_voltage(pm::LACRadPowerModel; kwargs...)
-end
-
 "Do nothing, no way to represent this in these variables"
 function _PM.constraint_theta_ref(pm::LACRadPowerModel, n::Int, ref_bus::Int)
 end
@@ -89,54 +85,6 @@ function _PM.constraint_power_balance(pm::LACRadPowerModel, n::Int, i, bus_arcs,
     end
 end
 
-# Copied from constraint_ne_power_balance(pm::AbstractWModels, n::Int, i::Int, bus_arcs, bus_arcs_dc, bus_arcs_sw, bus_arcs_ne, bus_gens, bus_storage, bus_pd, bus_qd, bus_gs, bus_bs)
-# in PowerModels/src/form/shared.jl, because AbstractWModels is a type union and cannot be subtyped
-""
-function _PM.constraint_ne_power_balance(pm::LACRadPowerModel, n::Int, i::Int, bus_arcs, bus_arcs_dc, bus_arcs_sw, bus_arcs_ne, bus_gens, bus_storage, bus_pd, bus_qd, bus_gs, bus_bs)
-    w    = var(pm, n, :w, i)
-    p    = get(var(pm, n),    :p, Dict()); _check_var_keys(p, bus_arcs, "active power", "branch")
-    q    = get(var(pm, n),    :q, Dict()); _check_var_keys(q, bus_arcs, "reactive power", "branch")
-    pg   = get(var(pm, n),   :pg, Dict()); _check_var_keys(pg, bus_gens, "active power", "generator")
-    qg   = get(var(pm, n),   :qg, Dict()); _check_var_keys(qg, bus_gens, "reactive power", "generator")
-    ps   = get(var(pm, n),   :ps, Dict()); _check_var_keys(ps, bus_storage, "active power", "storage")
-    qs   = get(var(pm, n),   :qs, Dict()); _check_var_keys(qs, bus_storage, "reactive power", "storage")
-    psw  = get(var(pm, n),  :psw, Dict()); _check_var_keys(psw, bus_arcs_sw, "active power", "switch")
-    qsw  = get(var(pm, n),  :qsw, Dict()); _check_var_keys(qsw, bus_arcs_sw, "reactive power", "switch")
-    p_dc = get(var(pm, n), :p_dc, Dict()); _check_var_keys(p_dc, bus_arcs_dc, "active power", "dcline")
-    q_dc = get(var(pm, n), :q_dc, Dict()); _check_var_keys(q_dc, bus_arcs_dc, "reactive power", "dcline")
-    p_ne = get(var(pm, n), :p_ne, Dict()); _check_var_keys(p_ne, bus_arcs_ne, "active power", "ne_branch")
-    q_ne = get(var(pm, n), :q_ne, Dict()); _check_var_keys(q_ne, bus_arcs_ne, "reactive power", "ne_branch")
-
-
-    cstr_p = JuMP.@constraint(pm.model,
-        sum(p[a] for a in bus_arcs)
-        + sum(p_dc[a_dc] for a_dc in bus_arcs_dc)
-        + sum(psw[a_sw] for a_sw in bus_arcs_sw)
-        + sum(p_ne[a] for a in bus_arcs_ne)
-        ==
-        sum(pg[g] for g in bus_gens)
-        - sum(ps[s] for s in bus_storage)
-        - sum(pd for pd in values(bus_pd))
-        - sum(gs for gs in values(bus_gs))*w
-    )
-    cstr_q = JuMP.@constraint(pm.model,
-        sum(q[a] for a in bus_arcs)
-        + sum(q_dc[a_dc] for a_dc in bus_arcs_dc)
-        + sum(qsw[a_sw] for a_sw in bus_arcs_sw)
-        + sum(q_ne[a] for a in bus_arcs_ne)
-        ==
-        sum(qg[g] for g in bus_gens)
-        - sum(qs[s] for s in bus_storage)
-        - sum(qd for qd in values(bus_qd))
-        + sum(bs for bs in values(bus_bs))*w
-    )
-
-    if _IM.report_duals(pm)
-        sol(pm, n, :bus, i)[:lam_kcl_r] = cstr_p
-        sol(pm, n, :bus, i)[:lam_kcl_i] = cstr_q
-    end
-end
-
 ""
 function _PM.constraint_ohms_yt_from(pm::LACRadPowerModel, n::Int, f_bus, t_bus, f_idx, t_idx, g, b, g_fr, b_fr, tr, ti, tm)
     p_fr = var(pm, n, :p, f_idx)
@@ -160,35 +108,8 @@ function _PM.constraint_ohms_yt_to(pm::LACRadPowerModel, n::Int, f_bus, t_bus, f
     JuMP.@constraint(pm.model, q_to  ==  - q_fr )
 end
 
-""
-function _PM.constraint_ne_ohms_yt_from(pm::LACRadPowerModel, n::Int, i, f_bus, t_bus, f_idx, t_idx, g, b, g_fr, b_fr, tr, ti, tm, vad_min, vad_max)
-        p_fr = var(pm, n, :p_ne, f_idx)
-    q_fr = var(pm, n, :q_ne, f_idx)
-    w_fr = var(pm, n, :w, f_bus)
-    w_to = var(pm, n, :w, t_bus)
-    z    = pinv(g + im * b)
-    r, x = real(z), imag(z)
-
-    JuMP.@constraint(pm.model, 2*r*p_fr + 2*x*q_fr ==  w_fr/tm^2 - w_to )
-end
-
-""
-function _PM.constraint_ne_ohms_yt_to(pm::LACRadPowerModel, n::Int, i, f_bus, t_bus, f_idx, t_idx, g, b, g_to, b_to, tr, ti, tm, vad_min, vad_max)
-    p_fr = var(pm, n, :p_ne, f_idx)
-    q_fr = var(pm, n, :q_ne, f_idx)
-    p_to = var(pm, n, :p_ne, t_idx)
-    q_to = var(pm, n, :q_ne, t_idx)
-
-    JuMP.@constraint(pm.model, p_to  ==  - p_fr )
-    JuMP.@constraint(pm.model, q_to  ==  - q_fr )
-end
-
 "nothing to do, no voltage angle variables"
 function _PM.constraint_voltage_angle_difference(pm::LACRadPowerModel, n::Int, f_idx, angmin, angmax)
-end
-
-"nothing to do, no voltage angle variables"
-function _PM.constraint_ne_voltage_angle_difference(pm::LACRadPowerModel, n::Int, f_idx, angmin, angmax, vad_min, vad_max)
 end
 
 "octagonal approximation of apparent power"
@@ -206,23 +127,6 @@ end
 
 "nothing to do, no line losses in this model"
 function _PM.constraint_thermal_limit_to(pm::LACRadPowerModel, n::Int, t_idx, rate_a)
-end
-
-"octagonal approximation of apparent power"
-function _PM.constraint_ne_thermal_limit_from(pm::LACRadPowerModel, n::Int, f_idx, rate_a)
-    p_fr = var(pm, n, :p_ne, f_idx)
-    q_fr = var(pm, n, :q_ne, f_idx)
-    c_perp = 0.9238795325112867 # == cos(π/8)
-    c_diag = 1.3065629648763766 # == sin(π/8) + cos(π/8) == cos(π/8) * sqrt(2)
-
-    JuMP.@constraint(pm.model, -c_perp*rate_a <= p_fr        <= c_perp*rate_a)
-    JuMP.@constraint(pm.model, -c_perp*rate_a <= q_fr        <= c_perp*rate_a)
-    JuMP.@constraint(pm.model, -c_diag*rate_a <= p_fr + q_fr <= c_diag*rate_a)
-    JuMP.@constraint(pm.model, -c_diag*rate_a <= p_fr - q_fr <= c_diag*rate_a)
-end
-
-"nothing to do, no line losses in this model"
-function _PM.constraint_ne_thermal_limit_to(pm::LACRadPowerModel, n::Int, t_idx, rate_a)
 end
 
 # Copied from constraint_switch_state_closed(pm::AbstractWModels, n::Int, f_bus, t_bus) in
