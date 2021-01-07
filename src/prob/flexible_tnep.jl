c2c827f3cfbac7b7c7fac2a0806979fe85651c1f
Matteo Rossini <matteo.rossini@rse-web.it>
2021-01-07 15:07:09 +0100

    Dropped DC equipment support in distribution networks

--- src/prob/flexible_tnep.jl
+++ src/prob/flexible_tnep.jl
@@ -17,10 +17,6 @@ function post_flex_tnep(pm::_PM.AbstractPowerModel)
         _PM.variable_gen_power(pm; nw = n)
         _PM.variable_branch_power(pm; nw = n)
         _PM.variable_storage_power(pm; nw = n)
-        if pm isa _PM.AbstractBFModel # distribution
-            _PM.variable_branch_current(pm; nw = n)
-            variable_oltc_branch_transform(pm; nw = n)
-        end
 
         _PMACDC.variable_voltage_slack(pm; nw = n)
         _PMACDC.variable_active_dcbranch_flow(pm; nw = n)
@@ -34,12 +30,7 @@ function post_flex_tnep(pm::_PM.AbstractPowerModel)
         # new variables for TNEP problem
         _PM.variable_ne_branch_indicator(pm; nw = n)
         _PM.variable_ne_branch_power(pm; nw = n)
-        if pm isa _PM.AbstractBFModel # distribution
-            variable_ne_branch_current(pm; nw = n)
-            variable_oltc_ne_branch_transform(pm; nw = n)
-        else # transmission
-            _PM.variable_ne_branch_voltage(pm; nw = n)
-        end
+        _PM.variable_ne_branch_voltage(pm; nw = n)
         variable_storage_power_ne(pm; nw = n)
         _PMACDC.variable_active_dcbranch_flow_ne(pm; nw = n)
         _PMACDC.variable_branch_ne(pm; nw = n)
@@ -51,13 +42,8 @@ function post_flex_tnep(pm::_PM.AbstractPowerModel)
     objective_min_cost_flex(pm)
 #CONSTRAINTS: defined within PowerModels(ACDC) can directly be used, other constraints need to be defined in the according sections of the code: flexible_demand.jl   
     for (n, networks) in pm.ref[:nw]
-        if pm isa _PM.AbstractBFModel # distribution
-            _PM.constraint_model_current(pm; nw = n)
-            constraint_ne_model_current(pm; nw = n)
-        else # transmission
-            _PM.constraint_model_voltage(pm; nw = n)
-            _PM.constraint_ne_model_voltage(pm; nw = n)
-        end
+        _PM.constraint_model_voltage(pm; nw = n)
+        _PM.constraint_ne_model_voltage(pm; nw = n)
         _PMACDC.constraint_voltage_dc(pm; nw = n)
         _PMACDC.constraint_voltage_dc_ne(pm; nw = n)
         for i in _PM.ids(pm, n, :ref_buses)
@@ -67,46 +53,30 @@ function post_flex_tnep(pm::_PM.AbstractPowerModel)
         for i in _PM.ids(pm, n, :bus)
             constraint_power_balance_acne_dcne_flex(pm, i; nw = n)
         end
-
-        if pm isa _PM.AbstractBFModel # distribution
+        if haskey(pm.setting, "allow_line_replacement") && pm.setting["allow_line_replacement"] == true
             for i in _PM.ids(pm, n, :branch)
-                constraint_dist_branch_tnep(pm, i; nw = n)
+                constraint_ohms_yt_from_repl(pm, i; nw = n)
+                constraint_ohms_yt_to_repl(pm, i; nw = n)
+                constraint_voltage_angle_difference_repl(pm, i; nw = n)
+                constraint_thermal_limit_from_repl(pm, i; nw = n)
+                constraint_thermal_limit_to_repl(pm, i; nw = n)
             end
-        else # transmission
-            if haskey(pm.setting, "allow_line_replacement") && pm.setting["allow_line_replacement"] == true
-                for i in _PM.ids(pm, n, :branch)
-                    constraint_ohms_yt_from_repl(pm, i; nw = n)
-                    constraint_ohms_yt_to_repl(pm, i; nw = n)
-                    constraint_voltage_angle_difference_repl(pm, i; nw = n)
-                    constraint_thermal_limit_from_repl(pm, i; nw = n)
-                    constraint_thermal_limit_to_repl(pm, i; nw = n)
-                end
-            else    
-                for i in _PM.ids(pm, n, :branch)
-                    _PM.constraint_ohms_yt_from(pm, i; nw = n)
-                    _PM.constraint_ohms_yt_to(pm, i; nw = n)
-                    _PM.constraint_voltage_angle_difference(pm, i; nw = n)
-                    _PM.constraint_thermal_limit_from(pm, i; nw = n)
-                    _PM.constraint_thermal_limit_to(pm, i; nw = n)
-                end
+        else    
+            for i in _PM.ids(pm, n, :branch)
+                _PM.constraint_ohms_yt_from(pm, i; nw = n)
+                _PM.constraint_ohms_yt_to(pm, i; nw = n)
+                _PM.constraint_voltage_angle_difference(pm, i; nw = n)
+                _PM.constraint_thermal_limit_from(pm, i; nw = n)
+                _PM.constraint_thermal_limit_to(pm, i; nw = n)
             end
         end
-
-        if pm isa _PM.AbstractBFModel # distribution
-            for i in _PM.ids(pm, n, :ne_branch)
-                constraint_dist_ne_branch_tnep(pm, i; nw = n)
-            end
-        else # transmission
-            for i in _PM.ids(pm, n, :ne_branch)
-                _PM.constraint_ne_ohms_yt_from(pm, i; nw = n)
-                _PM.constraint_ne_ohms_yt_to(pm, i; nw = n)
-                _PM.constraint_ne_voltage_angle_difference(pm, i; nw = n)
-                _PM.constraint_ne_thermal_limit_from(pm, i; nw = n)
-                _PM.constraint_ne_thermal_limit_to(pm, i; nw = n)
-            end
-        end
-        if n > 1
-            for i in _PM.ids(pm, n, :ne_branch) # both transmission and distribution
+        for i in _PM.ids(pm, n, :ne_branch)
+            _PM.constraint_ne_ohms_yt_from(pm, i; nw = n)
+            _PM.constraint_ne_ohms_yt_to(pm, i; nw = n)
+            _PM.constraint_ne_voltage_angle_difference(pm, i; nw = n)
+            _PM.constraint_ne_thermal_limit_from(pm, i; nw = n)
+            _PM.constraint_ne_thermal_limit_to(pm, i; nw = n)
+            if n > 1
                 _PMACDC.constraint_candidate_acbranches_mp(pm, n, i)
             end
         end
@@ -152,6 +122,133 @@ function post_flex_tnep(pm::_PM.AbstractPowerModel)
             if pm.ref[:nw][n][:convdc_ne][i]["islcc"] == 1
                 _PMACDC.constraint_conv_firing_angle_ne(pm, i; nw = n)
             end
+        end
+
+        for i in _PM.ids(pm, :load, nw = n)
+            if _PM.ref(pm, n, :load, i, "flex") == 0
+                constraint_fixed_demand(pm, i; nw = n)
+            else
+                constraint_flex_bounds_ne(pm, i; nw = n)
+            end
+            constraint_total_flexible_demand(pm, i; nw = n)
+        end
+
+        for i in _PM.ids(pm, :storage, nw=n)
+            constraint_storage_excl_slack(pm, i, nw = n)
+            _PM.constraint_storage_thermal_limit(pm, i, nw = n)
+            _PM.constraint_storage_losses(pm, i, nw = n)
+        end
+        for i in _PM.ids(pm, :ne_storage, nw=n)
+            constraint_storage_excl_slack_ne(pm, i, nw = n)
+            constraint_storage_thermal_limit_ne(pm, i, nw = n)
+            constraint_storage_losses_ne(pm, i, nw = n)
+            constraint_storage_bounds_ne(pm, i, nw = n)
+        end
+    end
+    network_ids = sort(collect(_PM.nw_ids(pm)))
+    n_1 = network_ids[1]
+    n_last = network_ids[end]
+    # NW = 1
+    for i in _PM.ids(pm, :storage, nw = n_1)
+        constraint_storage_state(pm, i, nw = n_1)
+        constraint_maximum_absorption(pm, i, nw = n_1)
+    end
+
+    for i in _PM.ids(pm, :ne_storage, nw = n_1)
+        constraint_storage_state_ne(pm, i, nw = n_1)
+        constraint_maximum_absorption_ne(pm, i, nw = n_1)
+    end
+
+    for i in _PM.ids(pm, :load, nw = n_1)
+        if _PM.ref(pm, n_1, :load, i, "flex") == 1
+            constraint_ence_state(pm, i, nw = n_1)
+            constraint_shift_up_state(pm, i, nw = n_1)
+            constraint_shift_down_state(pm, i, nw = n_1)
+        end
+    end
+    # NW = last
+    for i in _PM.ids(pm, :storage, nw = n_last)
+        constraint_storage_state_final(pm, i, nw = n_last)
+    end
+
+    for i in _PM.ids(pm, :ne_storage, nw = n_last)
+        constraint_storage_state_final_ne(pm, i, nw = n_last)
+    end
+
+    for i in _PM.ids(pm, :load, nw = n_last)
+        if _PM.ref(pm, n_last, :load, i, "flex") == 1
+            constraint_shift_state_final(pm, i, nw = n_last)
+        end
+    end
+
+    # NW = 2......last
+    for n_2 in network_ids[2:end]
+        for i in _PM.ids(pm, :storage, nw = n_2)
+            constraint_storage_state(pm, i, n_1, n_2)
+            constraint_maximum_absorption(pm, i, n_1, n_2)
+        end
+        for i in _PM.ids(pm, :ne_storage, nw = n_2)
+            constraint_storage_state_ne(pm, i, n_1, n_2)
+            constraint_maximum_absorption_ne(pm, i, n_1, n_2)
+            constraint_storage_investment(pm, n_1, n_2, i)
+        end
+        for i in _PM.ids(pm, :load, nw = n_2)
+            if _PM.ref(pm, n_2, :load, i, "flex") == 1
+                constraint_ence_state(pm, i, n_1, n_2)
+                constraint_shift_up_state(pm, n_1, n_2, i)
+                constraint_shift_down_state(pm, n_1, n_2, i) 
+                constraint_shift_duration(pm, n_2, i)
+                constraint_flex_investment(pm, n_1, n_2, i)
+            end
+        end
+        n_1 = n_2
+    end
+end
+
+# distribution version
+""
+function post_flex_tnep(pm::_PM.AbstractBFModel)
+
+    for (n, networks) in pm.ref[:nw]
+        _PM.variable_bus_voltage(pm; nw = n)
+        _PM.variable_gen_power(pm; nw = n)
+        _PM.variable_branch_power(pm; nw = n)
+        _PM.variable_storage_power(pm; nw = n)
+        _PM.variable_branch_current(pm; nw = n)
+        variable_oltc_branch_transform(pm; nw = n)
+
+        variable_absorbed_energy(pm; nw = n)
+        variable_absorbed_energy_ne(pm; nw = n)
+        variable_flexible_demand(pm; nw = n)
+
+        # new variables for TNEP problem
+        _PM.variable_ne_branch_indicator(pm; nw = n)
+        _PM.variable_ne_branch_power(pm; nw = n)
+        variable_ne_branch_current(pm; nw = n)
+        variable_oltc_ne_branch_transform(pm; nw = n)
+        variable_storage_power_ne(pm; nw = n)
+    end
+
+    objective_min_cost_flex(pm)
+
+    for (n, networks) in pm.ref[:nw]
+        _PM.constraint_model_current(pm; nw = n)
+        constraint_ne_model_current(pm; nw = n)
+
+        for i in _PM.ids(pm, n, :ref_buses)
+            _PM.constraint_theta_ref(pm, i, nw = n)
+        end
+
+        for i in _PM.ids(pm, n, :bus)
+            constraint_power_balance_acne_flex(pm, i; nw = n)
+        end
+
+        for i in _PM.ids(pm, n, :branch)
+            constraint_dist_branch_tnep(pm, i; nw = n)
+        end
+
+        for i in _PM.ids(pm, n, :ne_branch)
+            constraint_dist_ne_branch_tnep(pm, i; nw = n)
         end
 
         for i in _PM.ids(pm, :load, nw = n)
--- src/prob/storage_tnep.jl
+++ src/prob/storage_tnep.jl
@@ -12,15 +12,11 @@ end
 ""
 function post_strg_tnep(pm::_PM.AbstractPowerModel)
 # VARIABLES: defined within PowerModels(ACDC) can directly be used, other variables need to be defined in the according sections of the code: see storage.jl  
-    for (n, networks) in pm.ref[:nw]
+for (n, networks) in pm.ref[:nw]
         _PM.variable_bus_voltage(pm; nw = n)
         _PM.variable_gen_power(pm; nw = n)
         _PM.variable_branch_power(pm; nw = n)
         _PM.variable_storage_power(pm; nw = n)
-        if pm isa _PM.AbstractBFModel # distribution
-            _PM.variable_branch_current(pm; nw = n)
-            variable_oltc_branch_transform(pm; nw = n)
-        end
 
         _PMACDC.variable_voltage_slack(pm; nw = n)
         _PMACDC.variable_active_dcbranch_flow(pm; nw = n)
@@ -30,15 +26,11 @@ function post_strg_tnep(pm::_PM.AbstractPowerModel)
         variable_absorbed_energy(pm; nw = n)
         variable_absorbed_energy_ne(pm; nw = n)
 
+
         # new variables for TNEP problem
         _PM.variable_ne_branch_indicator(pm; nw = n)
         _PM.variable_ne_branch_power(pm; nw = n)
-        if pm isa _PM.AbstractBFModel # distribution
-            variable_ne_branch_current(pm; nw = n)
-            variable_oltc_ne_branch_transform(pm; nw = n)
-        else # transmission
-            _PM.variable_ne_branch_voltage(pm; nw = n)
-        end
+        _PM.variable_ne_branch_voltage(pm; nw = n)
         variable_storage_power_ne(pm; nw = n)
         _PMACDC.variable_active_dcbranch_flow_ne(pm; nw = n)
         _PMACDC.variable_branch_ne(pm; nw = n)
@@ -50,13 +42,8 @@ function post_strg_tnep(pm::_PM.AbstractPowerModel)
     objective_min_cost_storage(pm)
 #CONSTRAINTS: defined within PowerModels(ACDC) can directly be used, other constraints need to be defined in the according sections of the code: storage.jl 
     for (n, networks) in pm.ref[:nw]
-        if pm isa _PM.AbstractBFModel # distribution
-            _PM.constraint_model_current(pm; nw = n)
-            constraint_ne_model_current(pm; nw = n)
-        else # transmission
-            _PM.constraint_model_voltage(pm; nw = n)
-            _PM.constraint_ne_model_voltage(pm; nw = n)
-        end
+        _PM.constraint_model_voltage(pm; nw = n)
+        _PM.constraint_ne_model_voltage(pm; nw = n)
         _PMACDC.constraint_voltage_dc(pm; nw = n)
         _PMACDC.constraint_voltage_dc_ne(pm; nw = n)
         for i in _PM.ids(pm, n, :ref_buses)
@@ -66,46 +53,30 @@ function post_strg_tnep(pm::_PM.AbstractPowerModel)
         for i in _PM.ids(pm, n, :bus)
             constraint_power_balance_acne_dcne_strg(pm, i; nw = n)
         end
-
-        if pm isa _PM.AbstractBFModel # distribution
+        if haskey(pm.setting, "allow_line_replacement") && pm.setting["allow_line_replacement"] == true
             for i in _PM.ids(pm, n, :branch)
-                constraint_dist_branch_tnep(pm, i; nw = n)
+                constraint_ohms_yt_from_repl(pm, i; nw = n)
+                constraint_ohms_yt_to_repl(pm, i; nw = n)
+                constraint_voltage_angle_difference_repl(pm, i; nw = n)
+                constraint_thermal_limit_from_repl(pm, i; nw = n)
+                constraint_thermal_limit_to_repl(pm, i; nw = n)
             end
-        else # transmission
-            if haskey(pm.setting, "allow_line_replacement") && pm.setting["allow_line_replacement"] == true
-                for i in _PM.ids(pm, n, :branch)
-                    constraint_ohms_yt_from_repl(pm, i; nw = n)
-                    constraint_ohms_yt_to_repl(pm, i; nw = n)
-                    constraint_voltage_angle_difference_repl(pm, i; nw = n)
-                    constraint_thermal_limit_from_repl(pm, i; nw = n)
-                    constraint_thermal_limit_to_repl(pm, i; nw = n)
-                end
-            else    
-                for i in _PM.ids(pm, n, :branch)
-                    _PM.constraint_ohms_yt_from(pm, i; nw = n)
-                    _PM.constraint_ohms_yt_to(pm, i; nw = n)
-                    _PM.constraint_voltage_angle_difference(pm, i; nw = n)
-                    _PM.constraint_thermal_limit_from(pm, i; nw = n)
-                    _PM.constraint_thermal_limit_to(pm, i; nw = n)
-                end
+        else    
+            for i in _PM.ids(pm, n, :branch)
+                _PM.constraint_ohms_yt_from(pm, i; nw = n)
+                _PM.constraint_ohms_yt_to(pm, i; nw = n)
+                _PM.constraint_voltage_angle_difference(pm, i; nw = n)
+                _PM.constraint_thermal_limit_from(pm, i; nw = n)
+                _PM.constraint_thermal_limit_to(pm, i; nw = n)
             end
         end
-
-        if pm isa _PM.AbstractBFModel # distribution
-            for i in _PM.ids(pm, n, :ne_branch)
-                constraint_dist_ne_branch_tnep(pm, i; nw = n)
-            end
-        else # transmission
-            for i in _PM.ids(pm, n, :ne_branch)
-                _PM.constraint_ne_ohms_yt_from(pm, i; nw = n)
-                _PM.constraint_ne_ohms_yt_to(pm, i; nw = n)
-                _PM.constraint_ne_voltage_angle_difference(pm, i; nw = n)
-                _PM.constraint_ne_thermal_limit_from(pm, i; nw = n)
-                _PM.constraint_ne_thermal_limit_to(pm, i; nw = n)
-            end
-        end
-        if n > 1
-            for i in _PM.ids(pm, n, :ne_branch) # both transmission and distribution
+        for i in _PM.ids(pm, n, :ne_branch)
+            _PM.constraint_ne_ohms_yt_from(pm, i; nw = n)
+            _PM.constraint_ne_ohms_yt_to(pm, i; nw = n)
+            _PM.constraint_ne_voltage_angle_difference(pm, i; nw = n)
+            _PM.constraint_ne_thermal_limit_from(pm, i; nw = n)
+            _PM.constraint_ne_thermal_limit_to(pm, i; nw = n)
+            if n > 1
                 _PMACDC.constraint_candidate_acbranches_mp(pm, n, i)
             end
         end
@@ -151,6 +122,99 @@ function post_strg_tnep(pm::_PM.AbstractPowerModel)
             if pm.ref[:nw][n][:convdc_ne][i]["islcc"] == 1
                 _PMACDC.constraint_conv_firing_angle_ne(pm, i; nw = n)
             end
+        end
+
+        for i in _PM.ids(pm, :storage, nw=n)
+            constraint_storage_excl_slack(pm, i, nw = n)
+            _PM.constraint_storage_thermal_limit(pm, i, nw = n)
+            _PM.constraint_storage_losses(pm, i, nw = n)
+        end
+        for i in _PM.ids(pm, :ne_storage, nw=n)
+            constraint_storage_excl_slack_ne(pm, i, nw = n)
+            constraint_storage_thermal_limit_ne(pm, i, nw = n)
+            constraint_storage_losses_ne(pm, i, nw = n)
+            constraint_storage_bounds_ne(pm, i, nw = n)
+        end
+    end
+    network_ids = sort(collect(_PM.nw_ids(pm)))
+    n_1 = network_ids[1]
+    n_last = network_ids[end]
+    for i in _PM.ids(pm, :storage, nw = n_1)
+        constraint_storage_state(pm, i, nw = n_1)
+        constraint_maximum_absorption(pm, i, nw = n_1)
+    end
+
+    for i in _PM.ids(pm, :ne_storage, nw = n_1)
+        constraint_storage_state_ne(pm, i, nw = n_1)
+        constraint_maximum_absorption_ne(pm, i, nw = n_1)
+    end
+
+    for i in _PM.ids(pm, :storage, nw = n_last)
+        constraint_storage_state_final(pm, i, nw = n_last)
+    end
+
+    for i in _PM.ids(pm, :ne_storage, nw = n_last)
+        constraint_storage_state_final_ne(pm, i, nw = n_last)
+    end
+
+    for n_2 in network_ids[2:end]
+        for i in _PM.ids(pm, :storage, nw = n_2)
+            constraint_storage_state(pm, i, n_1, n_2)
+            constraint_maximum_absorption(pm, i, n_1, n_2)
+        end
+        for i in _PM.ids(pm, :ne_storage, nw = n_2)
+            constraint_storage_state_ne(pm, i, n_1, n_2)
+            constraint_maximum_absorption_ne(pm, i, n_1, n_2)
+            constraint_storage_investment(pm, n_1, n_2, i)
+        end
+        n_1 = n_2
+    end
+
+end
+
+# distribution version
+""
+function post_strg_tnep(pm::_PM.AbstractBFModel)
+
+    for (n, networks) in pm.ref[:nw]
+        _PM.variable_bus_voltage(pm; nw = n)
+        _PM.variable_gen_power(pm; nw = n)
+        _PM.variable_branch_power(pm; nw = n)
+        _PM.variable_storage_power(pm; nw = n)
+        _PM.variable_branch_current(pm; nw = n)
+        variable_oltc_branch_transform(pm; nw = n)
+
+        variable_absorbed_energy(pm; nw = n)
+        variable_absorbed_energy_ne(pm; nw = n)
+
+        # new variables for TNEP problem
+        _PM.variable_ne_branch_indicator(pm; nw = n)
+        _PM.variable_ne_branch_power(pm; nw = n)
+        variable_ne_branch_current(pm; nw = n)
+        variable_oltc_ne_branch_transform(pm; nw = n)
+        variable_storage_power_ne(pm; nw = n)
+    end
+
+    objective_min_cost_storage(pm)
+
+    for (n, networks) in pm.ref[:nw]
+        _PM.constraint_model_current(pm; nw = n)
+        constraint_ne_model_current(pm; nw = n)
+
+        for i in _PM.ids(pm, n, :ref_buses)
+            _PM.constraint_theta_ref(pm, i, nw = n)
+        end
+
+        for i in _PM.ids(pm, n, :bus)
+            constraint_power_balance_acne_strg(pm, i; nw = n)
+        end
+
+        for i in _PM.ids(pm, n, :branch)
+            constraint_dist_branch_tnep(pm, i; nw = n)
+        end
+
+        for i in _PM.ids(pm, n, :ne_branch)
+            constraint_dist_ne_branch_tnep(pm, i; nw = n)
         end
 
         for i in _PM.ids(pm, :storage, nw=n)
