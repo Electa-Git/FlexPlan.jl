fb63ff3f4fa504b11159f637f8645a73d986196d
espenb <espen.bodal@sintef.no>
2021-03-10 10:56:12 +0100

    fixed formulation of reliability objective and removed reliability specific code from post_flex_tnep

--- src/core/objective.jl
+++ src/core/objective.jl
@@ -106,92 +106,22 @@ end
 ##########################################################################
 ##################### Reliability objective with storage & flex candidates
 ##########################################################################
-function objective_reliability(pm::_PM.AbstractPowerModel)
-    gen_cost = Dict()
-    for (s, contingency) in pm.ref[:contingency]
-        for (sc, n) in contingency
-            for (i,gen) in pm.ref[:nw][n][:gen]
-                pg = _PM.var(pm, n, :pg, i)
-                if length(gen["cost"]) == 1
-                    gen_cost[(n,i)] = gen["cost"][1]
-                elseif length(gen["cost"]) == 2
-                    gen_cost[(n,i)] = (gen["cost"][1]*pg + gen["cost"][2])
-                elseif length(gen["cost"]) == 3
-                    gen_cost[(n,i)] = (gen["cost"][2]*pg + gen["cost"][3])
-                else
-                    gen_cost[(n,i)] = 0.0
-                end
-                if haskey(pm.setting, "add_co2_cost") && pm.setting["add_co2_cost"] == true
-                    gen_cost[(n,i)] = gen_cost[(n,i)] + gen["emission_factor"] * pg * pm.ref[:co2_emission_cost]
-                end
-            end
-        end
-    end
-    return JuMP.@objective(pm.model, Min,
-    sum(pm.ref[:contingency_prob][s] * 
-        sum(
-            sum(gen_cost[(n,i)] for (i,gen) in pm.ref[:nw][n][:gen]) + calculate_inv_cost(pm, n, is_contingency=true)
-            for (sc, n) in contingency # All times in a contingency scenario
-                ) 
-        for (s, contingency) in pm.ref[:contingency] # All contingency
-            )
-    )
-end
 
-function calculate_inv_cost(pm::_PM.AbstractPowerModel, n::Int; is_contingency::Bool=false)
-    if haskey(pm.setting, "add_co2_cost") && pm.setting["add_co2_cost"] == true
-        inv_cost = (
-            sum(conv["cost"]*_PM.var(pm, n, :conv_ne, i) for (i,conv) in pm.ref[:nw][n][:convdc_ne])
-            +
-            sum(conv["co2_cost"]*_PM.var(pm, n, :conv_ne, i) for (i,conv) in pm.ref[:nw][n][:convdc_ne])
-            +
-            sum(branch["construction_cost"]*_PM.var(pm, n, :branch_ne, i) for (i,branch) in pm.ref[:nw][n][:ne_branch])
-            +
-            sum(branch["co2_cost"]*_PM.var(pm, n, :branch_ne, i) for (i,branch) in pm.ref[:nw][n][:ne_branch])
-            +
-            sum(branch["cost"]*_PM.var(pm, n, :branchdc_ne, i) for (i,branch) in pm.ref[:nw][n][:branchdc_ne])
-            +
-            sum(branch["co2_cost"]*_PM.var(pm, n, :branchdc_ne, i) for (i,branch) in pm.ref[:nw][n][:branchdc_ne])
-            +
-            sum((storage["eq_cost"] + storage["inst_cost"] + storage["co2_cost"])*_PM.var(pm, n, :z_strg_ne, i) for (i,storage) in pm.ref[:nw][n][:ne_storage])
-            +
-            sum(load["cost_shift_up"]*_PM.var(pm, n, :pshift_up, i) for (i,load) in pm.ref[:nw][n][:load])
-            +
-            sum(load["cost_shift_down"]*_PM.var(pm, n, :pshift_down, i) for (i,load) in pm.ref[:nw][n][:load])
-            +
-            sum(load["cost_reduction"]*_PM.var(pm, n, :pnce, i) for (i,load) in pm.ref[:nw][n][:load])
-            +
-            sum(load["cost_curtailment"]*_PM.var(pm, n, :pcurt, i) for (i,load) in pm.ref[:nw][n][:load])
-            +
-            sum(load["co2_cost"]*_PM.var(pm, n, :z_flex, i) for (i,load) in pm.ref[:nw][n][:load])
-            )
-    else
-        inv_cost = (
-        sum(conv["cost"]*_PM.var(pm, n, :conv_ne, i) for (i,conv) in pm.ref[:nw][n][:convdc_ne])
-        +
-        sum(branch["construction_cost"]*_PM.var(pm, n, :branch_ne, i) for (i,branch) in pm.ref[:nw][n][:ne_branch])
-        +
-        sum(branch["cost"]*_PM.var(pm, n, :branchdc_ne, i) for (i,branch) in pm.ref[:nw][n][:branchdc_ne])
-        +
-        sum((storage["eq_cost"] + storage["inst_cost"])*_PM.var(pm, n, :z_strg_ne, i) for (i,storage) in pm.ref[:nw][n][:ne_storage])
-        +
-        sum(load["cost_shift_up"]*_PM.var(pm, n, :pshift_up, i) for (i,load) in pm.ref[:nw][n][:load])
-        +
-        sum(load["cost_shift_down"]*_PM.var(pm, n, :pshift_down, i) for (i,load) in pm.ref[:nw][n][:load])
-        +
-        sum(load["cost_reduction"]*_PM.var(pm, n, :pnce, i) for (i,load) in pm.ref[:nw][n][:load])
-        +
-        sum(load["cost_curtailment"]*_PM.var(pm, n, :pcurt, i) for (i,load) in pm.ref[:nw][n][:load])
-        +
-        sum(load["cost_investment"]*_PM.var(pm, n, :z_flex, i) for (i,load) in pm.ref[:nw][n][:load])
-        )
-    end
-    if is_contingency
-        if n ∉ [parse(Int, i) for i in keys(pm.ref[:contingency]["0"])]
-            inv_cost += sum(load["cost_voll"]*_PM.var(pm, n, :pinter, i) for (i,load) in pm.ref[:nw][n][:load])
-        end
-    end
-    return inv_cost
+function objective_reliability(pm::_PM.AbstractPowerModel)
+    add_co2_cost = haskey(pm.setting, "add_co2_cost") && pm.setting["add_co2_cost"]
+    return JuMP.@objective(pm.model, Min,
+        sum(pm.ref[:contingency_prob][s] * 
+            sum(
+                calc_gen_cost(pm, n, add_co2_cost)
+                + calc_convdc_ne_cost(pm, n, add_co2_cost)
+                + calc_ne_branch_cost(pm, n, add_co2_cost)
+                + calc_branchdc_ne_cost(pm, n, add_co2_cost)
+                + calc_ne_storage_cost(pm, n, add_co2_cost)
+                + calc_load_cost(pm, n, add_co2_cost)
+                + calc_contingency_cost(pm, n)
+            for (sc, n) in contingency)
+        for (s, contingency) in pm.ref[:contingency])
+    )
 end
 
 function calc_convdc_ne_cost(pm::_PM.AbstractPowerModel, n::Int, add_co2_cost::Bool)
@@ -265,6 +195,14 @@ function calc_load_cost(pm::_PM.AbstractPowerModel, n::Int, add_co2_cost::Bool)
             for (i,l) in load)
     if add_co2_cost
         cost += sum(l["co2_cost"]*_PM.var(pm, n, :z_flex, i) for (i,l) in load)
+    end
+    return cost
+end
+
+function calc_contingency_cost(pm::_PM.AbstractPowerModel, n::Int)
+    load = _PM.ref(pm, n, :load)
+    if n ∉ [parse(Int, t) for t in keys(pm.ref[:contingency]["0"])]
+        cost = sum(l["cost_voll"]*_PM.var(pm, n, :pinter, i) for (i,l) in load)
     end
     return cost
 end
