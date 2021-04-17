a8332d8f4f60ccc91eab600dec0c15cba20419f1
Iver Bakken Sperstad <51488720+ibsperstad@users.noreply.github.com>
2021-04-17 17:39:39 +0200

    Clean up comments in runscript for CIGRE MV demand flexibility case
    On fix_66_clean_up_demand_flexibility

--- test/scripts/tests_flex_demand_CIGRE_MV.jl
+++ test/scripts/tests_flex_demand_CIGRE_MV.jl
@@ -1,8 +1,15 @@
-#%%
+# TEST SCRIPT to run multi-period optimisation of demand flexibility for the CIGRE MV benchmark network
+# This code was used to produce Figures 53 to 56 in FlexPlan D1.2 (Sec. 9.3.2.2)
+# The code can be used to run either the linearized AC branch flow model for radial networks (as in D1.2)
+# or the DC power flow model by using the boolean switch parameter use_DC. 
+# If using the DC power flow there will be no grid issues (congestions) unless artificially forced using 
+# the boolean switch parameter do_force_congest.
+
 # Import relevant pakcages:
 # FlexPlan uses PowerModelsACDC for the multi-period transmission expansion optimisation & DC grid
 # PowerModelsACDC uses PowerModels for the AC grid, and the optimisation create_profile_data
 # InfrastructureModels is needed for data manipulation and common functions
+# IndexedTables is used for extracting results as tables to simplify plotting
 import FlexPlan; const _FP = FlexPlan
 import PowerModelsACDC; const _PMACDC = PowerModelsACDC
 import PowerModels; const _PM = PowerModels
@@ -10,29 +17,13 @@ import InfrastructureModels; const _IM = InfrastructureModels
 import IndexedTables; const _IT = IndexedTables
 using Plots
 
-# Add solver packages,, NOTE: packages are needed handle communication bwteeen solver and Julia/JuMP, 
-# they don't include the solver itself (the commercial ones). For instance ipopt, Cbc, juniper and so on should work
-#import Ipopt
-#import SCS
-#import Juniper
-#import Mosek
-#import MosekTools
+# Add solver packages (Cbc used here, but it could be replaced depending on which solvers are available)
 import JuMP
-#import Gurobi
 import Cbc
-#import CPLEX
 
 # Solver configurations
-#scs = JuMP.with_optimizer(SCS.Optimizer, max_iters=100000)
-#ipopt = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-4, print_level=0)
-#cplex = JuMP.with_optimizer(CPLEX.Optimizer)
 cbc = JuMP.with_optimizer(Cbc.Optimizer, tol=1e-4, seconds = 20, print_level=0)
-#gurobi = JuMP.with_optimizer(Gurobi.Optimizer)
-#mosek = JuMP.with_optimizer(Mosek.Optimizer)
-#juniper = JuMP.with_optimizer(Juniper.Optimizer, nl_solver = ipopt, mip_solver= cbc, time_limit= 7200)
 
-
-# TEST SCRIPT to run multi-period optimisation of demand flexibility for the CIGRE MV benchmark network
 
 # Input parameters:
 number_of_hours = 72          # Number of time steps
@@ -42,7 +33,7 @@ I_load_mon = 2:10                # The load point on which we monitor the load d
 I_bus_mon = 1:11              # The buses for which voltage magnitude is to be monitored
 I_load_other = []            # Load point for other loads on the same radial affecting congestion
 i_branch_mon = 2              # Index of branch on which to monitor congestion
-do_force_congest = false      # True if forcing congestion by modifying branch flow rating of i_branch_congest
+do_force_congest = false      # True if forcing congestion by modifying branch flow rating of i_branch_mon
 rate_congest = 16            # Rating of branch on which to force congestion
 load_scaling_factor = 0.8       # Factor with which original base case load demand data should be scaled
 use_DC = false                      # True for using DC power flow model; false for using linearized power real-reactive flow model for radial networks
@@ -87,11 +78,16 @@ if do_force_congest
 end
 
 if use_DC
-      _PMACDC.process_additional_data!(data) # Add DC grid data to the data dictionary
+      # Add DC grid data to the data dictionary
+      _PMACDC.process_additional_data!(data) 
 end
-_FP.add_flexible_demand_data!(data) # Add flexible data model
 
-extradata = _FP.create_profile_data(number_of_hours, data, loadprofile) # create a dictionary to pass time series data to data dictionary
+# Add flexible data model
+_FP.add_flexible_demand_data!(data) 
+
+# Create a dictionary to pass time series data to data dictionary
+extradata = _FP.create_profile_data(number_of_hours, data, loadprofile) 
+
 # Create data dictionary where time series data is included at the right place
 if use_DC
       mn_data = _PMACDC.multinetwork_data(data, extradata, Set{String}(["source_type", "name", "source_version", "per_unit"]))
@@ -112,30 +108,28 @@ else
 end
 
 # Build optimisation model, solve it and write solution dictionary:
-# This is the "problem file" which needs to be constructed individually depending on application
-# In this case: multi-period optimisation of demand flexibility, AC & DC lines and storage investments
 if use_DC
-      result_test1 = _FP.flex_tnep(mn_data, _PM.DCPPowerModel, cbc, multinetwork=true; setting = s)
+      result = _FP.flex_tnep(mn_data, _PM.DCPPowerModel, cbc, multinetwork=true; setting = s)
 else
-      result_test1 = _FP.flex_tnep(mn_data, _FP.BFARadPowerModel, cbc, multinetwork=true; setting = s)
+      result = _FP.flex_tnep(mn_data, _FP.BFARadPowerModel, cbc, multinetwork=true; setting = s)
 end
 
 # Printing to screen the branch investment decisions in the solution 
-for i_ne_branch = 1:length(result_test1["solution"]["nw"]["1"]["ne_branch"])
-      println("Is candidate branch ", i_ne_branch, " built: ", result_test1["solution"]["nw"]["1"]["ne_branch"][string(i_ne_branch)]["built"])
+for i_ne_branch = 1:length(result["solution"]["nw"]["1"]["ne_branch"])
+      println("Is candidate branch ", i_ne_branch, " built: ", result["solution"]["nw"]["1"]["ne_branch"][string(i_ne_branch)]["built"])
 end
 
 # Plot branch flow on congested branch
 if !isnan(i_branch_mon)
-      p_congest = _FP.plot_branch_flow(result_test1,i_branch_mon,data,"branch")
+      p_congest = _FP.plot_branch_flow(result,i_branch_mon,data,"branch")
       savefig(p_congest,"branch_flow_congest")
 end
 
 # Extract results for branch to monitor
-branch_mon = _FP.get_res(result_test1, "branch", string(i_branch_mon))
+branch_mon = _FP.get_res(result, "branch", string(i_branch_mon))
 
 # Extract results for new branch (assuming there only being one, and that it has a relevant placement)
-branch_new = _FP.get_res(result_test1, "ne_branch","2")
+branch_new = _FP.get_res(result, "ne_branch","2")
 
 # Extract results for load points to monitor 
 # (this code got quite ugly but I do not want to re-write/extend the get_vars functions right now...)
@@ -147,7 +141,7 @@ ence_load_mon = zeros(number_of_hours,1)
 pshift_up_load_mon = zeros(number_of_hours,1)
 pshift_down_load_mon = zeros(number_of_hours,1)
 for i_load_mon in I_load_mon
-      load_mon = _FP.get_res(result_test1, "load", string(i_load_mon))
+      load_mon = _FP.get_res(result, "load", string(i_load_mon))
       global pflex_load_mon += _IT.select(load_mon, :pflex)
       global pnce_load_mon += _IT.select(load_mon, :pnce)
       global pcurt_load_mon += _IT.select(load_mon, :pcurt)
@@ -161,21 +155,21 @@ end
 pflex_load_other = zeros(number_of_hours,1)
 pd_load_other = zeros(number_of_hours,1)
 for i_load_other in I_load_other
-      load_other = _FP.get_res(result_test1, "load", string(i_load_other))
+      load_other = _FP.get_res(result, "load", string(i_load_other))
       global pflex_load_other += _IT.select(load_other, :pflex)
       global pd_load_other += transpose(extradata["load"][string(i_load_other)]["pd"])
 end
 
 # Plot bus voltage magnitudes
 i_bus = I_bus_mon[1]
-voltage_plot = _FP.plot_res(result_test1,"bus",string(i_bus),"vm",label = string("bus ", i_bus), xlabel = "time (h)", ylabel = "voltage magnitude (p.u.)")
+voltage_plot = _FP.plot_res(result,"bus",string(i_bus),"vm",label = string("bus ", i_bus), xlabel = "time (h)", ylabel = "voltage magnitude (p.u.)")
 for i_bus in I_bus_mon[2:end]
-      _FP.plot_res!(result_test1,"bus",string(i_bus),"vm")
+      _FP.plot_res!(result,"bus",string(i_bus),"vm")
       voltage_plot.series_list[end].plotattributes[:label] = string("bus ", i_bus)
 end
 savefig(voltage_plot, "voltage.png")
 
-# Plot combined stacked area and line plot for energy balance in bus 5
+# Plot combined stacked area and line plot for energy balance across the monitored branch
 #... plot areas for power contribution from different sources
 branch_congest_flow = _IT.select(branch_mon, :pt)*-1
 if use_DC
@@ -193,7 +187,7 @@ load_flex = pflex_load_mon + pflex_load_other
 plot!(t_vec, load_flex, color=:blue, width=3.0, label="flexible demand", line=:dash)
 savefig(stacked_plot, "load_mod_balance.png")
 
-# Plot energy not served
+# Plot energy not served (i.e. energy not consumed according to the nomenclature in D1.2)
 plot_not_served = plot(t_vec, ence_load_mon, color=:black, width=3.0,
                            label="total energy not served", xlabel="time (h)",
                            ylabel="energy (MWh)", legend=false, gridalpha=0.5)
@@ -230,6 +224,7 @@ plot!([0], label = "flexible demand", color=:blue, line=:dash, width=3.0)
 _FP.stackedarea!([0],[0],label="load shift up", color=:blue)
 _FP.stackedarea!([0],[0], showaxis=false, grid=false, label="load shift down", legend=pos, color=:red,
              foreground_color_legend = nothing)
+
 # Add the two plots (energy balance and shifted demand) vertially
 #   and the legend on the side (with a given width -> .2w)
 vshift_plot = plot(stacked_plot, plot_energy_shift, v2legend, layout = @layout([[A; B] C{.2w}]),
