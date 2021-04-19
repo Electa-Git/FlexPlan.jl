04bdffc77c8fab4a03a7210859ce522b4131c0ec
Matteo Rossini <matteo.rossini@rse-web.it>
2021-04-19 11:47:04 +0200

    Improve `parse_data()`
    - If `ne_branch` table is missing, add an empty dict (an instance having zero candidate branches is fine).
    - If `load_extra` table is missing, throw an error (even if all loads are not flexible, `cost_curt` must be provided by the user; in distribution networks, `pf_angle` is mandatory too).
    - Improve documentation.

--- src/io/common.jl
+++ src/io/common.jl
@@ -4,25 +4,33 @@
 Parse a Matpower .m `file` or PTI (PSS(R)E-v33) .raw `file` into a FlexPlan data structure,
 including DC components, storage and flexible loads.
 
+Mandatory tables: `bus`, `gen`, `branch`, `load_extra`.
+Optional tables: `gencost`, `branch_oltc`, `storage`, `storage_extra`, `ne_storage`.
+Other tables can be added as well: they will be made avaiable in the returned object.
+
 Keyword arguments, if any, are forwarded to `PowerModels.parse_file()`.
 """
 function parse_file(file::String; kwargs...)
     data = _PM.parse_file(file; kwargs...)
+    if !haskey(data, "ne_branch")
+        data["ne_branch"] = Dict{String,Any}()
+    end
     if haskey(data, "busdc")
         _PMACDC.process_additional_data!(data)
     end
     add_storage_data!(data)
-    add_flexible_demand_data!(data)
+    if haskey(data, "load_extra")
+        add_flexible_demand_data!(data)
+    else
+        Memento.error(_LOGGER, "no load_extra table found in input file.")
+    end
     return data
 end
 
 """
     parse_file(file, scenario; <keyword arguments>)
 
-Parse a Matpower .m `file` or PTI (PSS(R)E-v33) .raw `file` into a FlexPlan data structure,
-including DC components, storage and flexible loads; scale cost data according to `scenario`.
-
-Keyword arguments, if any, are forwarded to `PowerModels.parse_file()`.
+Also scale cost data according to `scenario`.
 """
 function parse_file(file::String, scenario::Dict{String,Any}; kwargs...)
     data = parse_file(file; kwargs...)
