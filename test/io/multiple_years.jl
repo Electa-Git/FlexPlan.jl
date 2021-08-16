2de08f8a60122571cd4feeeb3984685e3dd52572
Matteo Rossini <matteo.rossini@rse-web.it>
2021-08-16 15:11:38 +0200

    Add `scale_factor` to metadata of `year` dimension
    - Add `scale_factor` to metadata of `year` dimension, to specify how many years each representative year should represent.
    - Remove `planning_horizon` from arguments of `create_multi_year_network_data()` since it should be determined by `case` argument.
    - Add `case` to arguments of `add_hours_and_scenarios()` so that the usage matches its definition in `test/io/multiple_years.jl`.
    - Update `test/scripts/test_multiple_years.jl`: reduce default test case size, use opensource solver as default, expose PowerModel variable.

--- test/io/multiple_years.jl
+++ test/io/multiple_years.jl
@@ -1,9 +1,9 @@
 """
     create_multi_year_network_data(case, number_of_hours, number_of_scenarios, number_of_years)
-    
-    Using the input case (case6, cigre, ...) create multi-year network data using the add_dimentions!(...). 
 
-    Dimension hierarchy is: 
+    Using the input case (case6, cigre, ...) create multi-year network data using the add_dimensions!(...).
+
+    Dimension hierarchy is:
     year{...
         scenario{...
             hour{...}
@@ -11,54 +11,55 @@
     }
 
 """
-function create_multi_year_network_data(case, number_of_hours, number_of_scenarios, number_of_years, planning_horizon)   
+function create_multi_year_network_data(case, number_of_hours, number_of_scenarios, number_of_years)
     if case == "case6"
-        ## Test case preparation
         base_file = "./test/data/multiple_years/case6/t_case6_"
         planning_years = [2030 2040 2050]
+        year_scale_factor = 10
         my_data = Dict{String, Any}()
-        for idx = 1 : number_of_years 
+        for idx = 1 : number_of_years
             year = planning_years[idx]
             file = join([base_file,"$year",".m"])
             data = _FP.parse_file(file)
-            data = add_hours_and_scenarios(case,data, number_of_hours, number_of_scenarios, planning_horizon)
+            data = add_hours_and_scenarios!(data, case, number_of_hours, number_of_scenarios, year_scale_factor)
             if idx == 1
-                _FP.add_dimension!(data, :year, number_of_years)
+                _FP.add_dimension!(data, :year, number_of_years; metadata = Dict{String,Any}("scale_factor"=>year_scale_factor))
                 my_data = data
             else
-                my_data = add_year(my_data, data)
+                my_data = add_year!(my_data, data)
             end
         end
     elseif case == "case67"
         base_file = "./test/data/multiple_years/case67/case67_tnep_"
         planning_years = [2030 2040 2050]
+        year_scale_factor = 10
         my_data = Dict{String, Any}()
-        for idx = 1 : number_of_years 
+        for idx = 1 : number_of_years
             year = planning_years[idx]
             file = join([base_file,"$year",".m"])
             data = _FP.parse_file(file)
-            data = add_hours_and_scenarios(case, data, number_of_hours, number_of_scenarios, planning_horizon; mc = false)
+            data = add_hours_and_scenarios!(data, case, number_of_hours, number_of_scenarios, year_scale_factor)
             if idx == 1
-                _FP.add_dimension!(data, :year, number_of_years)
+                _FP.add_dimension!(data, :year, number_of_years; metadata = Dict{String,Any}("scale_factor"=>year_scale_factor))
                 my_data = data
             else
-                my_data = add_year(my_data, data)
+                my_data = add_year!(my_data, data)
             end
         end
     else
-        error(join([case, " not (yet) supported"]))
+        error("Case \"$(case)\" not (yet) supported.")
     end
     return my_data
 end
 
-function add_hours_and_scenarios(case, data, number_of_hours, number_of_scenarios, planning_horizon; mc = true)
+function add_hours_and_scenarios!(data, case, number_of_hours, number_of_scenarios, year_scale_factor; kwargs...)
     if case == "case6"
         _FP.add_dimension!(data, :hour, number_of_hours)
 
         scenario = Dict(s => Dict{String,Any}("probability"=>1/number_of_scenarios) for s in 1:number_of_scenarios)
-        _FP.add_dimension!(data, :scenario, scenario, metadata = Dict{String,Any}("mc"=>mc))
+        _FP.add_dimension!(data, :scenario, scenario, metadata = Dict{String,Any}("mc"=>get(kwargs, :mc, true)))
 
-        _FP.scale_cost_data!(data, planning_horizon)
+        _FP.scale_cost_data!(data, year_scale_factor)
         data, loadprofile, genprofile = create_profile_data_italy!(data)
         extradata = _FP.create_profile_data(number_of_hours*number_of_scenarios, data, loadprofile, genprofile)
         data = _FP.multinetwork_data(data, extradata)
@@ -76,15 +77,17 @@ function add_hours_and_scenarios(case, data, number_of_hours, number_of_scenario
             scenario[s]["start"] = start[s]
         end
         _FP.add_dimension!(data, :scenario, scenario)
-        _FP.scale_cost_data!(data, planning_horizon)
+        _FP.scale_cost_data!(data, year_scale_factor)
         data, loadprofile, genprofile = create_profile_data_germany!(data)
         extradata = _FP.create_profile_data(number_of_hours*number_of_scenarios, data, loadprofile, genprofile)
         data = _FP.multinetwork_data(data, extradata)
+    else
+        error("Case \"$(case)\" not (yet) supported.")
     end
     return data
 end
 
-function add_year(my_data::Dict{String, Any}, data::Dict{String, Any})
+function add_year!(my_data::Dict{String, Any}, data::Dict{String, Any})
     number_of_networks = length(my_data["nw"])
     for (nw, network) in data["nw"]
         nw_id = parse(Int, nw) + number_of_networks
