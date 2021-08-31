9908c739f0448597570891256dbacb8d8bbb8fce
Matteo Rossini <matteo.rossini@rse-web.it>
2021-08-31 14:46:43 +0200

    Update objective functions
    Auxiliary functions that calculate investment costs now track the investment binary variable instead of the indicator variable: the cost is charged when the investment decision is taken.
    
    - `objective_stoch_flex`: updated.
    - `objective_min_cost_storage`: updated.
    - `objective_min_cost_flex`: updated transmission function; distribution function need to be updated after subnetworks are fixed.
    - `objective_reliability`: not updated (requires partial rewriting).
    
    For now, the residual value of investments at the end of the planning horizon is not taken into account, so all unit tests should pass after updating input files.

--- src/core/objective.jl
+++ src/core/objective.jl
@@ -3,15 +3,18 @@
 ##################################################################
 
 function objective_min_cost_storage(pm::_PM.AbstractPowerModel)
-    return JuMP.@objective(pm.model, Min,
-        sum(
-            calc_gen_cost(pm, n)
-            + calc_convdc_ne_cost(pm, n)
-            + calc_ne_branch_cost(pm, n)
-            + calc_branchdc_ne_cost(pm, n)
-            + calc_ne_storage_cost(pm, n)
-        for n in nw_ids(pm))
+    investment = sum(
+        calc_convdc_ne_cost(pm, n)
+        + calc_ne_branch_cost(pm, n)
+        + calc_branchdc_ne_cost(pm, n)
+        + calc_ne_storage_cost(pm, n)
+        for n in nw_ids(pm; hour=1)
     )
+    operation = sum(
+        calc_gen_cost(pm, n)
+        for n in nw_ids(pm)
+    )
+    JuMP.@objective(pm.model, Min, investment + operation)
 end
 
 
@@ -20,16 +23,20 @@ end
 ##################################################################
 
 function objective_min_cost_flex(pm::_PM.AbstractPowerModel)
-    return JuMP.@objective(pm.model, Min,
-        sum(
-            calc_gen_cost(pm, n)
-            + calc_convdc_ne_cost(pm, n)
-            + calc_ne_branch_cost(pm, n)
-            + calc_branchdc_ne_cost(pm, n)
-            + calc_ne_storage_cost(pm, n)
-            + calc_load_cost(pm, n)
-        for n in nw_ids(pm))
+    investment = sum(
+        calc_convdc_ne_cost(pm, n)
+        + calc_ne_branch_cost(pm, n)
+        + calc_branchdc_ne_cost(pm, n)
+        + calc_ne_storage_cost(pm, n)
+        + calc_load_investment_cost(pm, n)
+        for n in nw_ids(pm; hour=1)
     )
+    operation = sum(
+        calc_gen_cost(pm, n)
+        + calc_load_operational_cost(pm, n)
+        for n in nw_ids(pm)
+    )
+    JuMP.@objective(pm.model, Min, investment + operation)
 end
 
 function objective_min_cost_flex(t_pm::_PM.AbstractPowerModel, d_pm::_PM.AbstractPowerModel)
@@ -61,18 +68,23 @@ end
 ##########################################################################
 
 function objective_stoch_flex(pm::_PM.AbstractPowerModel)
-    return JuMP.@objective(pm.model, Min,
-        sum(scenario["probability"] *
-            sum(
-                calc_gen_cost(pm, n)
-                + calc_convdc_ne_cost(pm, n)
-                + calc_ne_branch_cost(pm, n)
-                + calc_branchdc_ne_cost(pm, n)
-                + calc_ne_storage_cost(pm, n)
-                + calc_load_cost(pm, n)
-            for n in nw_ids(pm; scenario=s))
-        for (s, scenario) in dim_prop(pm, :scenario))
+    investment = sum(
+        calc_convdc_ne_cost(pm, n)
+        + calc_ne_branch_cost(pm, n)
+        + calc_branchdc_ne_cost(pm, n)
+        + calc_ne_storage_cost(pm, n)
+        + calc_load_investment_cost(pm, n)
+        for n in nw_ids(pm; hour=1, scenario=1)
     )
+    operation = sum(scenario["probability"] *
+        sum(
+            calc_gen_cost(pm, n)
+            + calc_load_operational_cost(pm, n)
+            for n in nw_ids(pm; scenario=s)
+        )
+        for (s, scenario) in dim_prop(pm, :scenario)
+    )
+    JuMP.@objective(pm.model, Min, investment + operation)
 end
 
 
@@ -128,9 +140,9 @@ function calc_convdc_ne_cost(pm::_PM.AbstractPowerModel, n::Int)
     if haskey(_PM.ref(pm, n), :convdc_ne)
         convdc_ne = _PM.ref(pm, n, :convdc_ne)
         if !isempty(convdc_ne)
-            cost = sum(conv["cost"]*_PM.var(pm, n, :conv_ne, i) for (i,conv) in convdc_ne)
+            cost = sum(conv["cost"]*_PM.var(pm, n, :conv_ne_investment, i) for (i,conv) in convdc_ne)
             if get(pm.setting, "add_co2_cost", false)
-                cost += sum(conv["co2_cost"]*_PM.var(pm, n, :conv_ne, i) for (i,conv) in convdc_ne)
+                cost += sum(conv["co2_cost"]*_PM.var(pm, n, :conv_ne_investment, i) for (i,conv) in convdc_ne)
             end
         end
     end
@@ -142,9 +154,9 @@ function calc_ne_branch_cost(pm::_PM.AbstractPowerModel, n::Int)
     if haskey(_PM.ref(pm, n), :ne_branch)
         ne_branch = _PM.ref(pm, n, :ne_branch)
         if !isempty(ne_branch)
-            cost = sum(branch["construction_cost"]*_PM.var(pm, n, :branch_ne, i) for (i,branch) in ne_branch)
+            cost = sum(branch["construction_cost"]*_PM.var(pm, n, :branch_ne_investment, i) for (i,branch) in ne_branch)
             if get(pm.setting, "add_co2_cost", false)
-                cost += sum(branch["co2_cost"]*_PM.var(pm, n, :branch_ne, i) for (i,branch) in ne_branch)
+                cost += sum(branch["co2_cost"]*_PM.var(pm, n, :branch_ne_investment, i) for (i,branch) in ne_branch)
             end
         end
     end
@@ -156,9 +168,9 @@ function calc_branchdc_ne_cost(pm::_PM.AbstractPowerModel, n::Int)
     if haskey(_PM.ref(pm, n), :branchdc_ne)
         branchdc_ne = _PM.ref(pm, n, :branchdc_ne)
         if !isempty(branchdc_ne)
-            cost = sum(branch["cost"]*_PM.var(pm, n, :branchdc_ne, i) for (i,branch) in branchdc_ne)
+            cost = sum(branch["cost"]*_PM.var(pm, n, :branchdc_ne_investment, i) for (i,branch) in branchdc_ne)
             if get(pm.setting, "add_co2_cost", false)
-                cost += sum(branch["co2_cost"]*_PM.var(pm, n, :branchdc_ne, i) for (i,branch) in branchdc_ne)
+                cost += sum(branch["co2_cost"]*_PM.var(pm, n, :branchdc_ne_investment, i) for (i,branch) in branchdc_ne)
             end
         end
     end
@@ -170,9 +182,9 @@ function calc_ne_storage_cost(pm::_PM.AbstractPowerModel, n::Int)
     if haskey(_PM.ref(pm, n), :ne_storage)
         ne_storage = _PM.ref(pm, n, :ne_storage)
         if !isempty(ne_storage)
-            cost = sum((storage["eq_cost"] + storage["inst_cost"])*_PM.var(pm, n, :z_strg_ne, i) for (i,storage) in ne_storage)
+            cost = sum((storage["eq_cost"] + storage["inst_cost"])*_PM.var(pm, n, :z_strg_ne_investment, i) for (i,storage) in ne_storage)
             if get(pm.setting, "add_co2_cost", false)
-                cost += sum(storage["co2_cost"]*_PM.var(pm, n, :z_strg_ne, i) for (i,storage) in ne_storage)
+                cost += sum(storage["co2_cost"]*_PM.var(pm, n, :z_strg_ne_investment, i) for (i,storage) in ne_storage)
             end
         end
     end
@@ -186,19 +198,20 @@ end
 function calc_load_operational_cost(pm::_PM.AbstractPowerModel, n::Int)
     load = _PM.ref(pm, n, :load)
     cost = sum(
-            l["cost_shift_up"]*_PM.var(pm, n, :pshift_up, i)
-            + l["cost_shift_down"]*_PM.var(pm, n, :pshift_down, i)
-            + l["cost_reduction"]*_PM.var(pm, n, :pnce, i)
-            + l["cost_curtailment"]*_PM.var(pm, n, :pcurt, i)
-        for (i,l) in load)
+        l["cost_shift_up"]*_PM.var(pm, n, :pshift_up, i)
+        + l["cost_shift_down"]*_PM.var(pm, n, :pshift_down, i)
+        + l["cost_reduction"]*_PM.var(pm, n, :pnce, i)
+        + l["cost_curtailment"]*_PM.var(pm, n, :pcurt, i)
+        for (i,l) in load
+    )
     return cost
 end
 
 function calc_load_investment_cost(pm::_PM.AbstractPowerModel, n::Int)
     load = _PM.ref(pm, n, :load)
-    cost = sum(l["cost_investment"]*_PM.var(pm, n, :z_flex, i) for (i,l) in load)
+    cost = sum(l["cost_investment"]*_PM.var(pm, n, :z_flex_investment, i) for (i,l) in load)
     if get(pm.setting, "add_co2_cost", false)
-        cost += sum(l["co2_cost"]*_PM.var(pm, n, :z_flex, i) for (i,l) in load)
+        cost += sum(l["co2_cost"]*_PM.var(pm, n, :z_flex_investment, i) for (i,l) in load)
     end
     return cost
 end
