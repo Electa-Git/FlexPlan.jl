365fe59a3f139b134152078b5e6f29293105de96
Matteo Rossini <matteo.rossini@rse-web.it>
2021-09-24 09:39:41 +0200

    Change base name of binary variables related to DC branches
    By renaming `branch` to `branchdc`, these variables assume different names with respect to the corresponding variables related to AC branches.

--- src/core/variabledcgrid.jl
+++ src/core/variabledcgrid.jl
@@ -4,13 +4,13 @@ function variable_ne_branchdc_indicator(pm::_PM.AbstractPowerModel; nw::Int=pm.c
     if nw == first_n
         if !relax
             Z_dc_branch_ne = _PM.var(pm, nw)[:branchdc_ne] = JuMP.@variable(pm.model, #branch_ne is also name in PowerModels, branchdc_ne is candidate branches
-                [l in _PM.ids(pm, nw, :branchdc_ne)], base_name="$(nw)_branch_ne",
+                [l in _PM.ids(pm, nw, :branchdc_ne)], base_name="$(nw)_branchdc_ne",
                 binary = true,
                 start = _PM.comp_start_value(_PM.ref(pm, nw, :branchdc_ne, l), "convdc_tnep_start",  0.0)
             )
         else
             Z_dc_branch_ne = _PM.var(pm, nw)[:branchdc_ne] = JuMP.@variable(pm.model, #branch_ne is also name in PowerModels, branchdc_ne is candidate branches
-                [l in _PM.ids(pm, nw, :branchdc_ne)], base_name="$(nw)_branch_ne",
+                [l in _PM.ids(pm, nw, :branchdc_ne)], base_name="$(nw)_branchdc_ne",
                 lower_bound = 0,
                 upper_bound = 1,
                 start = _PM.comp_start_value(_PM.ref(pm, nw, :branchdc_ne, l), "convdc_tnep_start",  0.0)
@@ -27,13 +27,13 @@ function variable_ne_branchdc_investment(pm::_PM.AbstractPowerModel; nw::Int=pm.
     if nw == first_n
         if !relax
             investment = _PM.var(pm, nw)[:branchdc_ne_investment] = JuMP.@variable(pm.model, #branch_ne is also name in PowerModels, branchdc_ne is candidate branches
-                [l in _PM.ids(pm, nw, :branchdc_ne)], base_name="$(nw)_branch_ne_investment",
+                [l in _PM.ids(pm, nw, :branchdc_ne)], base_name="$(nw)_branchdc_ne_investment",
                 binary = true,
                 start = 0
             )
         else
             investment = _PM.var(pm, nw)[:branchdc_ne_investment] = JuMP.@variable(pm.model, #branch_ne is also name in PowerModels, branchdc_ne is candidate branches
-                [l in _PM.ids(pm, nw, :branchdc_ne)], base_name="$(nw)_branch_ne_investment",
+                [l in _PM.ids(pm, nw, :branchdc_ne)], base_name="$(nw)_branchdc_ne_investment",
                 lower_bound = 0,
                 upper_bound = 1,
                 start = 0
