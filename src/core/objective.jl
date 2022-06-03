fbb7ad7c61c13a7294b189e7705696e0e6eb33fd
Matteo Rossini <matteo.rossini@rse-web.it>
2022-06-03 07:37:51 +0200

    Improve formulation of objective functions
    - Improve speed and memory requirement by avoiding addition operator outside JuMP macros (<https://jump.dev/JuMP.jl/stable/tutorials/getting_started/performance_tips/#Use-macros-to-build-expressions>)
    - Delete `calc_load_cost` as it is unused
    - Rename `calc_load_operational_cost` to `calc_load_operation_cost`

--- src/core/objective.jl
+++ src/core/objective.jl
@@ -1,269 +1,255 @@
 ## Objective with candidate storage
 
 function objective_min_cost_storage(pm::_PM.AbstractPowerModel)
-    investment = sum(
-        calc_convdc_ne_cost(pm, n)
-        + calc_ne_branch_cost(pm, n)
-        + calc_branchdc_ne_cost(pm, n)
-        + calc_ne_storage_cost(pm, n)
-        for n in nw_ids(pm; hour=1)
-    )
-    operation = sum(
-        calc_gen_cost(pm, n)
-        for n in nw_ids(pm)
-    )
-    JuMP.@objective(pm.model, Min, investment + operation)
+    cost = JuMP.AffExpr(0.0)
+    # Investment cost
+    for n in nw_ids(pm; hour=1)
+        JuMP.add_to_expression!(cost, calc_convdc_ne_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_branch_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_branchdc_ne_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_storage_cost(pm,n))
+    end
+    # Operation cost
+    for n in nw_ids(pm)
+        JuMP.add_to_expression!(cost, calc_gen_cost(pm,n))
+    end
+    JuMP.@objective(pm.model, Min, cost)
 end
 
 function objective_min_cost_storage(t_pm::_PM.AbstractPowerModel, d_pm::_PM.AbstractPowerModel)
-    t_investment = sum(
-        calc_convdc_ne_cost(t_pm, n)
-        + calc_ne_branch_cost(t_pm, n)
-        + calc_branchdc_ne_cost(t_pm, n)
-        + calc_ne_storage_cost(t_pm, n)
-        for n in nw_ids(t_pm; hour=1)
-    )
-    t_operation = sum(
-        calc_gen_cost(t_pm, n)
-        for n in nw_ids(t_pm)
-    )
-    d_investment = sum( # Note: distribution networks do not have DC components (modeling decision)
-        calc_ne_branch_cost(d_pm, n)
-        + calc_ne_storage_cost(d_pm, n)
-        for n in nw_ids(d_pm; hour=1)
-    )
-    d_operation = sum(
-        calc_gen_cost(d_pm, n)
-        for n in nw_ids(d_pm)
-    )
-    JuMP.@objective(t_pm.model, Min, t_investment + t_operation + d_investment + d_operation) # Note: t_pm.model == d_pm.model
+    cost = JuMP.AffExpr(0.0)
+    # Transmission investment cost
+    for n in nw_ids(t_pm; hour=1)
+        JuMP.add_to_expression!(cost, calc_convdc_ne_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_branch_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_branchdc_ne_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_storage_cost(t_pm,n))
+    end
+    # Transmission operation cost
+    for n in nw_ids(t_pm)
+        JuMP.add_to_expression!(cost, calc_gen_cost(t_pm,n))
+    end
+    # Distribution investment cost
+    for n in nw_ids(d_pm; hour=1)
+        # Note: distribution networks do not have DC components (modeling decision)
+        JuMP.add_to_expression!(cost, calc_ne_branch_cost(d_pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_storage_cost(d_pm,n))
+    end
+    # Distribution operation cost
+    for n in nw_ids(d_pm)
+        JuMP.add_to_expression!(cost, calc_gen_cost(d_pm,n))
+    end
+    JuMP.@objective(t_pm.model, Min, cost) # Note: t_pm.model == d_pm.model
 end
 
 
 ## Objective with candidate storage and flexible demand
 
 function objective_min_cost_flex(pm::_PM.AbstractPowerModel)
-    investment = sum(
-        calc_convdc_ne_cost(pm, n)
-        + calc_ne_branch_cost(pm, n)
-        + calc_branchdc_ne_cost(pm, n)
-        + calc_ne_storage_cost(pm, n)
-        + calc_load_investment_cost(pm, n)
-        for n in nw_ids(pm; hour=1)
-    )
-    operation = sum(
-        calc_gen_cost(pm, n)
-        + calc_load_operational_cost(pm, n)
-        for n in nw_ids(pm)
-    )
-    JuMP.@objective(pm.model, Min, investment + operation)
+    cost = JuMP.AffExpr(0.0)
+    # Investment cost
+    for n in nw_ids(pm; hour=1)
+        JuMP.add_to_expression!(cost, calc_convdc_ne_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_branch_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_branchdc_ne_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_storage_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_load_investment_cost(pm,n))
+    end
+    # Operation cost
+    for n in nw_ids(pm)
+        JuMP.add_to_expression!(cost, calc_gen_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_load_operation_cost(pm,n))
+    end
+    JuMP.@objective(pm.model, Min, cost)
 end
 
 function objective_min_cost_flex(t_pm::_PM.AbstractPowerModel, d_pm::_PM.AbstractPowerModel)
-    t_investment = sum(
-        calc_convdc_ne_cost(t_pm, n)
-        + calc_ne_branch_cost(t_pm, n)
-        + calc_branchdc_ne_cost(t_pm, n)
-        + calc_ne_storage_cost(t_pm, n)
-        + calc_load_investment_cost(t_pm, n)
-        for n in nw_ids(t_pm; hour=1)
-    )
-    t_operation = sum(
-        calc_gen_cost(t_pm, n)
-        + calc_load_operational_cost(t_pm, n)
-        for n in nw_ids(t_pm)
-    )
-    d_investment = sum( # Note: distribution networks do not have DC components (modeling decision)
-        calc_ne_branch_cost(d_pm, n)
-        + calc_ne_storage_cost(d_pm, n)
-        + calc_load_investment_cost(d_pm, n)
-        for n in nw_ids(d_pm; hour=1)
-    )
-    d_operation = sum(
-        calc_gen_cost(d_pm, n)
-        + calc_load_operational_cost(d_pm, n)
-        for n in nw_ids(d_pm)
-    )
-    JuMP.@objective(t_pm.model, Min, t_investment + t_operation + d_investment + d_operation) # Note: t_pm.model == d_pm.model
+    cost = JuMP.AffExpr(0.0)
+    # Transmission investment cost
+    for n in nw_ids(t_pm; hour=1)
+        JuMP.add_to_expression!(cost, calc_convdc_ne_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_branch_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_branchdc_ne_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_storage_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_load_investment_cost(t_pm,n))
+    end
+    # Transmission operation cost
+    for n in nw_ids(t_pm)
+        JuMP.add_to_expression!(cost, calc_gen_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_load_operation_cost(t_pm,n))
+    end
+    # Distribution investment cost
+    for n in nw_ids(d_pm; hour=1)
+        # Note: distribution networks do not have DC components (modeling decision)
+        JuMP.add_to_expression!(cost, calc_ne_branch_cost(d_pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_storage_cost(d_pm,n))
+        JuMP.add_to_expression!(cost, calc_load_investment_cost(d_pm,n))
+    end
+    # Distribution operation cost
+    for n in nw_ids(d_pm)
+        JuMP.add_to_expression!(cost, calc_gen_cost(d_pm,n))
+        JuMP.add_to_expression!(cost, calc_load_operation_cost(d_pm,n))
+    end
+    JuMP.@objective(t_pm.model, Min, cost) # Note: t_pm.model == d_pm.model
 end
 
 
 ## Stochastic objective with candidate storage and flexible demand
 
 function objective_stoch_flex(pm::_PM.AbstractPowerModel)
-    investment = sum(
-        calc_convdc_ne_cost(pm, n)
-        + calc_ne_branch_cost(pm, n)
-        + calc_branchdc_ne_cost(pm, n)
-        + calc_ne_storage_cost(pm, n)
-        + calc_load_investment_cost(pm, n)
-        for n in nw_ids(pm; hour=1, scenario=1)
-    )
-    operation = sum(scenario["probability"] *
-        sum(
-            calc_gen_cost(pm, n)
-            + calc_load_operational_cost(pm, n)
-            for n in nw_ids(pm; scenario=s)
-        )
-        for (s, scenario) in dim_prop(pm, :scenario)
-    )
-    JuMP.@objective(pm.model, Min, investment + operation)
+    cost = JuMP.AffExpr(0.0)
+    # Investment cost
+    for n in nw_ids(pm; hour=1, scenario=1)
+        JuMP.add_to_expression!(cost, calc_convdc_ne_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_branch_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_branchdc_ne_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_storage_cost(pm,n))
+        JuMP.add_to_expression!(cost, calc_load_investment_cost(pm,n))
+    end
+    # Operation cost
+    for (s, scenario) in dim_prop(pm, :scenario)
+        scenario_probability = scenario["probability"]
+        for n in nw_ids(pm; scenario=s)
+            JuMP.add_to_expression!(cost, scenario_probability, calc_gen_cost(pm,n))
+            JuMP.add_to_expression!(cost, scenario_probability, calc_load_operation_cost(pm,n))
+        end
+    end
+    JuMP.@objective(pm.model, Min, cost)
 end
 
 function objective_stoch_flex(t_pm::_PM.AbstractPowerModel, d_pm::_PM.AbstractPowerModel)
-    t_investment = sum(
-        calc_convdc_ne_cost(t_pm, n)
-        + calc_ne_branch_cost(t_pm, n)
-        + calc_branchdc_ne_cost(t_pm, n)
-        + calc_ne_storage_cost(t_pm, n)
-        + calc_load_investment_cost(t_pm, n)
-        for n in nw_ids(t_pm; hour=1, scenario=1)
-    )
-    t_operation = sum(scenario["probability"] *
-        sum(
-            calc_gen_cost(t_pm, n)
-            + calc_load_operational_cost(t_pm, n)
-            for n in nw_ids(t_pm; scenario=s)
-        )
-        for (s, scenario) in dim_prop(t_pm, :scenario)
-    )
-    d_investment = sum( # Note: distribution networks do not have DC components (modeling decision)
-        calc_ne_branch_cost(d_pm, n)
-        + calc_ne_storage_cost(d_pm, n)
-        + calc_load_investment_cost(d_pm, n)
-        for n in nw_ids(d_pm; hour=1, scenario=1)
-    )
-    d_operation = sum(scenario["probability"] *
-        sum(
-            calc_gen_cost(d_pm, n)
-            + calc_load_operational_cost(d_pm, n)
-            for n in nw_ids(d_pm; scenario=s)
-        )
-        for (s, scenario) in dim_prop(d_pm, :scenario)
-    )
-    JuMP.@objective(t_pm.model, Min, t_investment + t_operation + d_investment + d_operation) # Note: t_pm.model == d_pm.model
+    cost = JuMP.AffExpr(0.0)
+    # Transmission investment cost
+    for n in nw_ids(t_pm; hour=1, scenario=1)
+        JuMP.add_to_expression!(cost, calc_convdc_ne_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_branch_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_branchdc_ne_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_storage_cost(t_pm,n))
+        JuMP.add_to_expression!(cost, calc_load_investment_cost(t_pm,n))
+    end
+    # Transmission operation cost
+    for (s, scenario) in dim_prop(t_pm, :scenario)
+        scenario_probability = scenario["probability"]
+        for n in nw_ids(t_pm; scenario=s)
+            JuMP.add_to_expression!(cost, scenario_probability, calc_gen_cost(t_pm,n))
+            JuMP.add_to_expression!(cost, scenario_probability, calc_load_operation_cost(t_pm,n))
+        end
+    end
+    # Distribution investment cost
+    for n in nw_ids(d_pm; hour=1, scenario=1)
+        # Note: distribution networks do not have DC components (modeling decision)
+        JuMP.add_to_expression!(cost, calc_ne_branch_cost(d_pm,n))
+        JuMP.add_to_expression!(cost, calc_ne_storage_cost(d_pm,n))
+        JuMP.add_to_expression!(cost, calc_load_investment_cost(d_pm,n))
+    end
+    # Distribution operation cost
+    for (s, scenario) in dim_prop(d_pm, :scenario)
+        scenario_probability = scenario["probability"]
+        for n in nw_ids(d_pm; scenario=s)
+            JuMP.add_to_expression!(cost, scenario_probability, calc_gen_cost(d_pm,n))
+            JuMP.add_to_expression!(cost, scenario_probability, calc_load_operation_cost(d_pm,n))
+        end
+    end
+    JuMP.@objective(t_pm.model, Min, cost) # Note: t_pm.model == d_pm.model
 end
 
 
 ## Auxiliary functions
 
 function calc_gen_cost(pm::_PM.AbstractPowerModel, n::Int)
-
-    function calc_single_gen_cost(i, g_cost)
-        len = length(g_cost)
-        cost = 0.0
-        if len >= 1
-            cost = g_cost[len] # Constant term
-            if len >= 2
-                cost += g_cost[len-1] * _PM.var(pm,n,:pg,i) # Adds linear term
-            end
+    cost = JuMP.AffExpr(0.0)
+    for (i,g) in _PM.ref(pm, n, :gen)
+        if length(g["cost"]) ≥ 2
+            JuMP.add_to_expression!(cost, g["cost"][end-1], _PM.var(pm,n,:pg,i))
         end
-        return cost
     end
-
-    gen = _PM.ref(pm, n, :gen)
-    cost = sum(calc_single_gen_cost(i,g["cost"]) for (i,g) in gen)
-    dgen = _PM.ref(pm, n, :dgen)
-    if get(pm.setting, "add_co2_cost", false) && !isempty(dgen)
-        cost += sum(g["emission_factor"] * _PM.var(pm,n,:pg,i) * pm.ref[:it][_PM.pm_it_sym][:co2_emission_cost] for (i,g) in dgen)
+    if get(pm.setting, "add_co2_cost", false)
+        co2_emission_cost = pm.ref[:it][_PM.pm_it_sym][:co2_emission_cost]
+        for (i,g) in _PM.ref(pm, n, :dgen)
+            JuMP.add_to_expression!(cost, g["emission_factor"]*co2_emission_cost, _PM.var(pm,n,:pg,i))
+        end
     end
-    ndgen = _PM.ref(pm, n, :ndgen)
-    if !isempty(ndgen)
-        cost += sum(g["cost_curt"] * _PM.var(pm,n,:pgcurt,i) for (i,g) in ndgen)
+    for (i,g) in _PM.ref(pm, n, :ndgen)
+        JuMP.add_to_expression!(cost, g["cost_curt"], _PM.var(pm,n,:pgcurt,i))
     end
     return cost
 end
 
 function calc_convdc_ne_cost(pm::_PM.AbstractPowerModel, n::Int)
-    cost = 0.0
-    if haskey(_PM.ref(pm, n), :convdc_ne)
-        convdc_ne = _PM.ref(pm, n, :convdc_ne)
-        if !isempty(convdc_ne)
-            cost = sum(conv["cost"]*_PM.var(pm, n, :conv_ne_investment, i) for (i,conv) in convdc_ne)
-            if get(pm.setting, "add_co2_cost", false)
-                cost += sum(conv["co2_cost"]*_PM.var(pm, n, :conv_ne_investment, i) for (i,conv) in convdc_ne)
-            end
+    add_co2_cost = get(pm.setting, "add_co2_cost", false)
+    cost = JuMP.AffExpr(0.0)
+    for (i,conv) in get(_PM.ref(pm,n), :convdc_ne, Dict())
+        conv_cost = conv["cost"]
+        if add_co2_cost
+            conv_cost += conv["co2_cost"]
         end
+        JuMP.add_to_expression!(cost, conv_cost, _PM.var(pm,n,:conv_ne_investment,i))
     end
     return cost
 end
 
 function calc_ne_branch_cost(pm::_PM.AbstractPowerModel, n::Int)
-    cost = 0.0
-    if haskey(_PM.ref(pm, n), :ne_branch)
-        ne_branch = _PM.ref(pm, n, :ne_branch)
-        if !isempty(ne_branch)
-            cost = sum(branch["construction_cost"]*_PM.var(pm, n, :branch_ne_investment, i) for (i,branch) in ne_branch)
-            if get(pm.setting, "add_co2_cost", false)
-                cost += sum(branch["co2_cost"]*_PM.var(pm, n, :branch_ne_investment, i) for (i,branch) in ne_branch)
-            end
+    add_co2_cost = get(pm.setting, "add_co2_cost", false)
+    cost = JuMP.AffExpr(0.0)
+    for (i,branch) in get(_PM.ref(pm,n), :ne_branch, Dict())
+        branch_cost = branch["construction_cost"]
+        if add_co2_cost
+            branch_cost += branch["co2_cost"]
         end
+        JuMP.add_to_expression!(cost, branch_cost, _PM.var(pm,n,:branch_ne_investment,i))
     end
     return cost
 end
 
 function calc_branchdc_ne_cost(pm::_PM.AbstractPowerModel, n::Int)
-    cost = 0.0
-    if haskey(_PM.ref(pm, n), :branchdc_ne)
-        branchdc_ne = _PM.ref(pm, n, :branchdc_ne)
-        if !isempty(branchdc_ne)
-            cost = sum(branch["cost"]*_PM.var(pm, n, :branchdc_ne_investment, i) for (i,branch) in branchdc_ne)
-            if get(pm.setting, "add_co2_cost", false)
-                cost += sum(branch["co2_cost"]*_PM.var(pm, n, :branchdc_ne_investment, i) for (i,branch) in branchdc_ne)
-            end
+    add_co2_cost = get(pm.setting, "add_co2_cost", false)
+    cost = JuMP.AffExpr(0.0)
+    for (i,branch) in get(_PM.ref(pm,n), :branchdc_ne, Dict())
+        branch_cost = branch["cost"]
+        if add_co2_cost
+            branch_cost += branch["co2_cost"]
         end
+        JuMP.add_to_expression!(cost, branch_cost, _PM.var(pm,n,:branchdc_ne_investment,i))
     end
     return cost
 end
 
 function calc_ne_storage_cost(pm::_PM.AbstractPowerModel, n::Int)
-    cost = 0.0
-    if haskey(_PM.ref(pm, n), :ne_storage)
-        ne_storage = _PM.ref(pm, n, :ne_storage)
-        if !isempty(ne_storage)
-            cost = sum((storage["eq_cost"] + storage["inst_cost"])*_PM.var(pm, n, :z_strg_ne_investment, i) for (i,storage) in ne_storage)
-            if get(pm.setting, "add_co2_cost", false)
-                cost += sum(storage["co2_cost"]*_PM.var(pm, n, :z_strg_ne_investment, i) for (i,storage) in ne_storage)
-            end
+    add_co2_cost = get(pm.setting, "add_co2_cost", false)
+    cost = JuMP.AffExpr(0.0)
+    for (i,storage) in get(_PM.ref(pm,n), :ne_storage, Dict())
+        storage_cost = storage["eq_cost"] + storage["inst_cost"]
+        if add_co2_cost
+            storage_cost += storage["co2_cost"]
         end
+        JuMP.add_to_expression!(cost, storage_cost, _PM.var(pm,n,:z_strg_ne_investment,i))
     end
     return cost
 end
 
-function calc_load_cost(pm::_PM.AbstractPowerModel, n::Int)
-    return calc_load_operational_cost(pm, n) + calc_load_investment_cost(pm, n)
-end
-
-function calc_load_operational_cost(pm::_PM.AbstractPowerModel, n::Int)
-    cost = 0.0
-    flex_load = _PM.ref(pm, n, :flex_load)
-    if !isempty(flex_load)
-        cost += sum(
-            0.5 * l["cost_shift"] * _PM.var(pm, n, :pshift_up, i) # Splitting into half and half allows for better cost attribution when running single-period problems or problems with no integral constraints.
-            + 0.5 * l["cost_shift"] * _PM.var(pm, n, :pshift_down, i)
-            + l["cost_red"] * _PM.var(pm, n, :pred, i)
-            + l["cost_curt"] * _PM.var(pm, n, :pcurt, i)
-            for (i,l) in flex_load
-        )
+function calc_load_operation_cost(pm::_PM.AbstractPowerModel, n::Int)
+    cost = JuMP.AffExpr(0.0)
+    for (i,l) in _PM.ref(pm, n, :flex_load)
+        JuMP.add_to_expression!(cost, 0.5*l["cost_shift"], _PM.var(pm,n,:pshift_up,i)) # Splitting into half and half allows for better cost attribution when running single-period problems or problems with no integral constraints.
+        JuMP.add_to_expression!(cost, 0.5*l["cost_shift"], _PM.var(pm,n,:pshift_down,i))
+        JuMP.add_to_expression!(cost, l["cost_red"], _PM.var(pm,n,:pred,i))
+        JuMP.add_to_expression!(cost, l["cost_curt"], _PM.var(pm,n,:pcurt,i))
     end
-    fixed_load = _PM.ref(pm, n, :fixed_load)
-    if !isempty(fixed_load)
-        cost += sum(
-            l["cost_curt"]*_PM.var(pm, n, :pcurt, i)
-            for (i,l) in fixed_load
-        )
+    for (i,l) in _PM.ref(pm, n, :fixed_load)
+        JuMP.add_to_expression!(cost, l["cost_curt"], _PM.var(pm,n,:pcurt,i))
     end
     return cost
 end
 
 function calc_load_investment_cost(pm::_PM.AbstractPowerModel, n::Int)
-    cost = 0.0
-    flex_load = _PM.ref(pm, n, :flex_load)
-    if !isempty(flex_load)
-        cost = sum(l["cost_inv"]*_PM.var(pm, n, :z_flex_investment, i) for (i,l) in flex_load)
-        if get(pm.setting, "add_co2_cost", false)
-            cost += sum(l["co2_cost"]*_PM.var(pm, n, :z_flex_investment, i) for (i,l) in flex_load)
+    add_co2_cost = get(pm.setting, "add_co2_cost", false)
+    cost = JuMP.AffExpr(0.0)
+    for (i,l) in _PM.ref(pm, n, :flex_load)
+        load_cost = l["cost_inv"]
+        if add_co2_cost
+            load_cost += l["co2_cost"]
         end
+        JuMP.add_to_expression!(cost, load_cost, _PM.var(pm,n,:z_flex_investment,i))
     end
     return cost
 end
