a628322a8d56637a3b387940718f1e7752f669b6
Matteo Rossini <matteo.rossini@rse-web.it>
2022-10-19 16:34:51 +0200

    Fix manual resolution of merge conflicts

--- src/FlexPlan.jl
+++ src/FlexPlan.jl
@@ -54,6 +54,7 @@ include("core/constraint.jl")
 include("core/line_replacement.jl")
 include("core/distribution.jl")
 include("core/td_coupling.jl")
+include("core/solution.jl")
 
 include("form/bf.jl")
 include("form/bfarad.jl")
